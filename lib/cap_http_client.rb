3e82334dacddb344f64d9bd41ed7c4e9711c7ac6
Willy Mene <wmene@stanford.edu>
2013-08-03 18:16:14 -0700

    Grab a Cap Authorship Profile

--- lib/cap_http_client.rb
+++ lib/cap_http_client.rb
@@ -8,11 +8,13 @@ class CapHttpClient
     else
       @auth = @main_auth[:development]
     end
+    @base_timeout_retries = 3
+    @base_timeout_period = 500
   end
 
 	def get_new_token
 		begin
-  		http = Net::HTTP.new(auth[:get_token_uri], auth[:get_token_port])
+  		http = Net::HTTP.new(@auth[:get_token_uri], @auth[:get_token_port])
 			http.use_ssl = true
 			http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			http.ssl_version = :SSLv3
@@ -44,24 +46,25 @@ class CapHttpClient
 
   	end
 
-  	def get_batch_from_cap_api(page_count, page_size, since='')
-  		#since = "2013-06-14T10:33:20.333-04:00"
-  		json_response = {}
-  		begin
-  			timeout_retries ||= 3
-  			timeout_period ||= 500
-	  		auth = YAML.load(File.open(Rails.root.join('config', 'cap_auth.yaml')))
-			#AlwaysVerifySSLCertificates.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt'
-			http = Net::HTTP.new(@auth[:authorship_api_uri], @auth[:authorship_api_port])
-			http.read_timeout = timeout_period
-			http.use_ssl = true
-			http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-			#http.ca_file = '/opt/local/share/curl/curl-ca-bundle.crt'
-		#	http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-			http.ssl_version = :SSLv3
-			request_path = "#{@auth[:authorship_api_path]}?p=#{page_count}&ps=#{page_size}"
-			unless since.blank? then request_path << "&since=#{since}" end
-			request = Net::HTTP::Get.new(request_path)
+  def get_batch_from_cap_api(page_count, page_size, since='')
+    request_path = "#{@auth[:authorship_api_path]}?p=#{page_count}&ps=#{page_size}"
+		request_path << "&since=#{since}" unless since.blank?
+    make_cap_request(request_path)
+  end
+
+  def get_auth_profile(cap_profile_id)
+    make_cap_request("#{@auth[:authorship_api_path]}/#{cap_profile_id}")
+  end
+
+private
+  def make_cap_request(request_path)
+    json_response = {}
+    timeout_retries = @base_timeout_retries
+    timeout_period = @base_timeout_period
+
+    begin
+      http = setup_cap_http
+      request = Net::HTTP::Get.new(request_path)
 			#puts "bearer " + @auth[:token].to_s
 			token = @auth[:access_token]
 
@@ -81,23 +84,31 @@ class CapHttpClient
 					response_body = response.body
 					json_response = JSON.parse(response_body)
 					http.finish
-					break
 				end
 			end
-		rescue Timeout::Error => te
-			timeout_retries -= 1
-			if timeout_retries > 0
-				# increase timeout
-				timeout_period =+ 500
-				retry
-			else
-				NotificationManager.handle_authorship_pull_error(te, "Timeout error on authorship call - #{DateTime.now}" )
-				raise
-			end
-		rescue => e
-			NotificationManager.handle_authorship_pull_error(e, "Problem with http call to cap authorship api")
-			raise
-		end
-		json_response
-  	end
+    rescue Timeout::Error => te
+      timeout_retries -= 1
+      if timeout_retries > 0
+        # increase timeout
+        timeout_period =+ 500
+        retry
+      else
+        NotificationManager.handle_harvest_problem(te, "Timeout error on authorship call - #{DateTime.now}" )
+        raise
+      end
+    rescue => e
+      NotificationManager.handle_harvest_problem(e, "Problem with http call to cap authorship api")
+      raise
+    end
+    json_response
+  end
+
+  def setup_cap_http
+    http = Net::HTTP.new(@auth[:authorship_api_uri], @auth[:authorship_api_port])
+    http.read_timeout = @base_timeout_period
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.ssl_version = :SSLv3
+    http
+  end
 end
