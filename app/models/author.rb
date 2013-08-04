4ca9074d30981b56b56fa997dfb97df3b2e6d9d9
Willy Mene <wmene@stanford.edu>
2013-08-03 18:15:45 -0700

    Create an Author from the Cap Authorship API profile hash

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,27 +1,62 @@
 class Author < ActiveRecord::Base
-  attr_accessible :cap_profile_id, :sunetid, :university_id, :california_physician_license, :active_in_cap, :cap_import_enabled, :emails_for_harvest, :email, :cap_first_name, :cap_last_name, :cap_middle_name, :official_first_name, :official_last_name, :official_middle_name, :preferred_first_name, :preferred_last_name, :preferred_middle_name 
+  attr_accessible :cap_profile_id, :sunetid, :university_id, :california_physician_license, :active_in_cap, :cap_import_enabled, :emails_for_harvest, :email, :cap_first_name, :cap_last_name, :cap_middle_name, :official_first_name, :official_last_name, :official_middle_name, :preferred_first_name, :preferred_last_name, :preferred_middle_name
   has_many :contributions, :dependent => :destroy
   has_many :publications, :through => :contributions do
   	def approved
   		where("contributions.status='approved'")
   	end
   end
-  
-  has_many  :approved_sw_ids, :through => :contributions, 
-          :class_name => "PublicationIdentifier", 
+
+  has_many  :approved_sw_ids, :through => :contributions,
+          :class_name => "PublicationIdentifier",
           :source => :publication_identifier,
           :foreign_key => "publication_id",
           :primary_key => "publication_id",
           :conditions => "contributions.status = 'approved'"
 
-  has_many  :approved_publications, :through => :contributions, 
-          :class_name => "Publication", 
+  has_many  :approved_publications, :through => :contributions,
+          :class_name => "Publication",
           :source => :publication,
-          :conditions => ['contributions.status = ?','approved'] 
-         
+          :conditions => ['contributions.status = ?','approved']
+
 
   #has_many :population_memberships, :dependent => :destroy
   #has_many :author_identifiers, :dependent => :destroy
 
+  def Author.create_from_cap_authorship_profile_hash(auth_hash)
+
+    # key/value not present in hash if value is not there
+    # sunetid/ university id/ ca licence ---- at least one will be there
+    seed_hash = {
+      cap_profile_id: auth_hash['profileId'],
+      active_in_cap:  auth_hash['active'],
+      cap_import_enabled: auth_hash['importEnabled']
+    }
+
+    Author.add_to_hash_if_present(seed_hash, :sunetid, auth_hash['profile']['uid'])
+    Author.add_to_hash_if_present(seed_hash, :university_id, auth_hash['profile']['universityId'])
+    Author.add_to_hash_if_present(seed_hash, :email, auth_hash['profile']['email'])
+    Author.add_to_hash_if_present(seed_hash, :emails_for_harvest, auth_hash['profile']['email'])
+    Author.add_to_hash_if_present(seed_hash, :official_first_name, auth_hash['profile']['names']['legal']['firstName'])
+    Author.add_to_hash_if_present(seed_hash, :official_middle_name, auth_hash['profile']['names']['legal']['middleName'])
+    Author.add_to_hash_if_present(seed_hash, :official_last_name, auth_hash['profile']['names']['legal']['lastName'])
+    Author.add_to_hash_if_present(seed_hash, :cap_first_name, auth_hash['profile']['names']['preferred']['firstName'])
+    Author.add_to_hash_if_present(seed_hash, :cap_middle_name, auth_hash['profile']['names']['preferred']['middleName'])
+    Author.add_to_hash_if_present(seed_hash, :cap_last_name, auth_hash['profile']['names']['preferred']['lastName'])
+    Author.add_to_hash_if_present(seed_hash, :preferred_first_name, auth_hash['profile']['names']['preferred']['firstName'])
+    Author.add_to_hash_if_present(seed_hash, :preferred_middle_name, auth_hash['profile']['names']['preferred']['middleName'])
+    Author.add_to_hash_if_present(seed_hash, :preferred_last_name, auth_hash['profile']['names']['preferred']['lastName'])
+    Author.add_to_hash_if_present(seed_hash, :california_physician_license, auth_hash['profile']['californiaPhysicianLicense'])
+
+    Author.create seed_hash
+  end
+
+  def Author.add_to_hash_if_present(seed_hash, key, value)
+    if(value.nil?)
+      seed_hash[key] = ''
+    else
+      seed_hash[key] = value
+    end
+  end
 
 end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,112 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Author do
+
+  let(:auth_hash) {
+    {"active"=>false,
+     "authorship"=>
+      [{"featured"=>false,
+        "status"=>"new",
+        "sulPublicationId"=>198465,
+        "visibility"=>"public"}],
+     "importEnabled"=>false,
+     "importSettings"=>
+       [{"firstName"=>"Henry", "lastName"=>"Lowe", "middleName"=>"J."},
+        {"firstName"=>"H",
+         "institution"=>"Stanford University",
+         "lastName"=>"Lowe",
+         "middleName"=>"J"}],
+      "lastModified"=>"2013-08-03T06:12:25.767-07:00",
+      "populations"=>["stanford", "som", "shc"],
+      "profile"=>
+       {"californiaPhysicianLicense"=>"C50735",
+        "displayName"=>"Henry J. Lowe, MD",
+        "email"=>"hlowe@stanford.edu",
+        "meta"=>
+         {"etag"=>"17810123",
+          "links"=>
+           [{"href"=>"http://irt-ppapp-02:12011/cap-api-qa/api/profiles/v1/3810",
+             "rel"=>"https://cap.stanford.edu/rel/self"},
+            {"href"=>"https://irt-dev.stanford.edu/cap-su-qa/henry-lowe",
+             "rel"=>"https://cap.stanford.edu/rel/public"},
+            {"href"=>
+              "https://irt-dev.stanford.edu/profiles-qa/auth/frdActionServlet?choiceId=facProfile&profileId=3810",
+             "rel"=>"https://cap.stanford.edu/rel/intranet"},
+            {"href"=>
+              "https://irt-dev.stanford.edu/profiles-qa/frdActionServlet?choiceId=printerprofile&profileversion=full&profileId=3810",
+             "rel"=>"https://cap.stanford.edu/rel/pdf"}]},
+        "names"=>
+         {"legal"=>{"firstName"=>"Henry", "lastName"=>"Lowe", "middleName"=>"J"},
+          "preferred"=>
+           {"firstName"=>"Henry", "lastName"=>"Lowe", "middleName"=>"J."}},
+        "profileId"=>3810,
+        "uid"=>"hlowe",
+        "universityId"=>"09724972"},
+      "profileId"=>3810,
+      "visibility"=>"public"}
+  }
+
+  let(:missing_fields) {
+    {"active"=>false,
+     "authorship"=>
+      [{"featured"=>false,
+        "status"=>"new",
+        "sulPublicationId"=>198465,
+        "visibility"=>"public"}],
+     "importEnabled"=>false,
+     "importSettings"=>
+       [{"firstName"=>"Henry", "lastName"=>"Lowe", "middleName"=>"J."},
+        {"firstName"=>"H",
+         "institution"=>"Stanford University",
+         "lastName"=>"Lowe",
+         "middleName"=>"J"}],
+      "lastModified"=>"2013-08-03T06:12:25.767-07:00",
+      "populations"=>["stanford", "som", "shc"],
+      "profile"=>
+       {"californiaPhysicianLicense"=>"C50735",
+        "displayName"=>"Henry J. Lowe, MD",
+        "meta"=>
+         {"etag"=>"17810123",
+          "links"=>
+           [{"href"=>"http://irt-ppapp-02:12011/cap-api-qa/api/profiles/v1/3810",
+             "rel"=>"https://cap.stanford.edu/rel/self"},
+            {"href"=>"https://irt-dev.stanford.edu/cap-su-qa/henry-lowe",
+             "rel"=>"https://cap.stanford.edu/rel/public"},
+            {"href"=>
+              "https://irt-dev.stanford.edu/profiles-qa/auth/frdActionServlet?choiceId=facProfile&profileId=3810",
+             "rel"=>"https://cap.stanford.edu/rel/intranet"},
+            {"href"=>
+              "https://irt-dev.stanford.edu/profiles-qa/frdActionServlet?choiceId=printerprofile&profileversion=full&profileId=3810",
+             "rel"=>"https://cap.stanford.edu/rel/pdf"}]},
+        "names"=>
+         {"legal"=>{"firstName"=>"Henry", "lastName"=>"Lowe", "middleName"=>"J"},
+          "preferred"=>
+           {"firstName"=>"Henry", "lastName"=>"Lowe"}},
+        "profileId"=>3810,
+        "uid"=>"hlowe",
+        "universityId"=>"09724972"},
+      "profileId"=>3810,
+      "visibility"=>"public"}
+  }
+
+  describe ".create_from_cap_authorship_profile_hash" do
+
+    it "creates an author from the profile JSON returned from the CAP authorship API" do
+      auth = Author.create_from_cap_authorship_profile_hash(auth_hash)
+      auth.cap_profile_id.should == auth_hash['profileId']
+      auth.cap_last_name.should == auth_hash['profile']['names']['preferred']['lastName']
+      auth.sunetid.should == auth_hash['profile']['uid']
+      #...
+    end
+
+    it "creates an author froma hash with missing fields" do
+      auth = Author.create_from_cap_authorship_profile_hash(missing_fields)
+      auth.email.should be_blank
+      auth.preferred_middle_name.should be_blank
+      auth.email.should be_blank
+      auth.emails_for_harvest.should be_blank
+    end
+
+  end
+
+end
