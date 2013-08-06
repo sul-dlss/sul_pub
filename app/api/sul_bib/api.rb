5548a05ba07ccbb2212d240cfc3b6aee1535297e
Chris Beer <cabeer@stanford.edu>
2013-08-06 13:22:41 -0700

    add info/debug logging to publications api

--- app/api/sul_bib/api.rb
+++ app/api/sul_bib/api.rb
@@ -4,10 +4,18 @@ require 'sul_bib/publications_api'
 
 module SulBib
   class API < Grape::API
+    logger Rails.logger
 
     before do
       error!('Unauthorized', 401) unless env['HTTP_CAPKEY'] == SulBib::API_KEY
     end
+
+    helpers do
+      def logger
+        API.logger
+      end
+    end
+
 
     group (:authorship) { mount SulBib::AuthorshipAPI }
     group (:publications) { mount SulBib::PublicationsAPI }
--- app/api/sul_bib/publications_api.rb
+++ app/api/sul_bib/publications_api.rb
@@ -5,6 +5,7 @@ module SulBib
     format :json
 
     helpers do
+
       def wrap_as_bibjson_collection(description, query, records, page, per_page)
         metadata = {
               _created: Time.now.iso8601,
@@ -47,12 +48,14 @@ module SulBib
       optional :max_rows, type: Integer, default: 20
     end
     get :sourcelookup do
+      logger.info("Executing source lookup for title #{params[:title]} with sources #{params[:source]}")
       all_matching_records = []
 
       sources = params[:source].split('+')
 
       if sources.include?(Settings.sciencewire_source)
         all_matching_records += ScienceWireClient.new.query_sciencewire_for_publication(params[:firstname], params[:lastname], params[:middlename], params[:title], params[:year], params[:max_rows])
+        logger.debug(" -- sciencewire (#{all_matching_records.length})")
       end
 
       if sources.include?(Settings.manual_source)
@@ -63,6 +66,7 @@ module SulBib
         if params[:year]
           results = results.where(user_submitted_source_records[:year].eq(params[:year]))          
         end
+        logger.debug(" -- manual source (#{results.length})")
 
         all_matching_records += results.map {|source_record| source_record.publication.pub_hash }
       end
@@ -81,6 +85,12 @@ module SulBib
       optional :per, type: Integer, default: 100
     end
     get do
+      logger.info("Getting all publications" +
+         ((" for profile #{params[:capProfileId]}" if params[:capProfileId]) || "") +
+         ((" where capActive = #{params[:capActive]}" if params[:capActive]) || "") +
+         ((" limit to changedSize #{params[:changedSince]}" if params[:changedSince]) || "")
+      )
+
       matching_records = []
 
       capProfileId = params[:capProfileId]
@@ -91,22 +101,26 @@ module SulBib
 
 
       if capProfileId.blank?
+        logger.debug(" -- CAP Profile ID not provided, returning all records modified after #{last_changed}")
         description = "Records that have changed since #{last_changed}"
 
         query = Publication.updated_after(last_changed)
 
         if !capActive.blank? && capActive.downcase == 'true'
+          logger.debug(" -- Limit to only active authors")
           query = query.with_active_author
         end
 
         matching_records = query.order('publications.id').page(page).per(per).pluck(:pub_hash)
-
+        logger.debug("Found #{matching_records.length} records")
       else
+        logger.debug("Limited to only CAP Profile ID #{capProfileId}")
         author = Author.where(cap_profile_id: capProfileId).first
         if author.nil?
           error!({ "error" => "No such author", "detail" => "You've specified a non-existant author." }, 404)
         else
           description = "All known publications for CAP profile id " + capProfileId
+          logger.debug("Limited to all publications for author #{author.inspect}")
           matching_records = author.publications.order('publications.id').page(page).per(per).pluck('publications.pub_hash')
         end
       end
@@ -121,11 +135,14 @@ module SulBib
     end
     post do
       request_body_unparsed = env['api.request.input']
+      logger.info("adding new manual publication from BibJSON")
+      logger.debug("#{request_body_unparsed}")
       pub_hash = params[:pub_hash]
      # Rails.logger.debug "Incoming bibjson post attributes hash: #{pub_hash}"
       fingerprint = Digest::SHA2.hexdigest(request_body_unparsed)
       existingRecord = UserSubmittedSourceRecord.where(source_fingerprint: fingerprint).first
-      unless existingRecord.nil?
+      if existingRecord
+        logger.info("Found existing record for #{fingerprint}: #{existingRecord.inspect}; redirecting.")
           # the GRAPE redirect method issues a 303 (when original request is not a get, and a 302 for a get)
           # and sets the location header to the specified url
           # So, this next lines return 303 with location equal to the pub's uri
@@ -135,6 +152,7 @@ module SulBib
           error!("You haven't supplied a valid authorship record.", 406)
         end
         pub = Publication.build_new_manual_publication(Settings.cap_provenance, pub_hash, request_body_unparsed)
+        logger.debug("Created new publication #{pub.inspect}")
         header "Location", env["REQUEST_URI"].to_s + "/" + pub.id.to_s
         pub.pub_hash
       end
@@ -150,7 +168,7 @@ module SulBib
     put ':id' do
       #the last known etag must be sent in the 'if-match' header, returning 412 “Precondition Failed” if etags don't match,
       #and a 428 "Precondition Required" if the if-match header isn't supplied
-
+      
       begin
         pub = Publication.find(params[:id])
       rescue ActiveRecord::RecordNotFound
@@ -171,9 +189,12 @@ module SulBib
       end
 
       original_source = env['api.request.input']
-      pub.update_manual_pub_from_pub_hash(params[:pub_hash], Settings.cap_provenance, original_source)
-      pub.pub_hash
+      logger.info("Update manual publication #{pub.inspect} with BibJSON")
+      logger.debug("#{original_source}")
 
+      pub.update_manual_pub_from_pub_hash(params[:pub_hash], Settings.cap_provenance, original_source)
+      logger.debug("resulting pub hash: #{pub.pub_hash}")
+      pub.pub_hash
     end
 
     desc "GET A SINGLE RECORD"
