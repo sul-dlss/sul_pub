571e40fba5538eef99a0f1cac449b9e0189c4e4b
Willy Mene <wmene@stanford.edu>
2013-09-11 22:57:17 -0700

    Tweaks to the delete_missing_auths script.
    Get the set of pub_ids of the deleted author instead
    of loading the publications

--- script/delete_missing_auths.rb
+++ script/delete_missing_auths.rb
@@ -9,16 +9,24 @@ class DeleteMissingAuths
 
   def fix(auth_id)
     auth = Author.find auth_id
-    pubs = auth.publications
-
-    orphan_pubs = pubs.select {|p| p.contributions.size == 1}
-    orphan_pubs.each do |p|
-      @logger.info "   Destroying orphan pub #{p.id}"
-      p.destroy
-    end
+    pub_ids = auth.publications.pluck(:id)
 
     @logger.info "Destroying author #{auth.id}"
     auth.destroy
+    pub_ids.each do |pub_id|
+      pub = Publication.find pub_id
+      if pub.contributions.size == 0
+        @logger.info "   Destroying orphan pub #{pub_id}"
+        pub.destroy
+      else
+        @logger.info "   Resynching pub #{pub_id}"
+        pub.set_last_updated_value_in_hash
+        pub.add_all_db_contributions_to_my_pub_hash
+        pub.save
+      end
+    end
+
+
   rescue ActiveRecord::RecordNotFound => e
     @logger.warn "Author id not found #{auth_id}"
   end
