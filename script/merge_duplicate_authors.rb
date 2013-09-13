8cd0f2508b95b92b0823989816e84888091baa53
Willy Mene <wmene@stanford.edu>
2013-09-13 13:15:36 -0700

    Adding script to merge duplicate authors

---
+++ script/merge_duplicate_authors.rb
@@ -0,0 +1,71 @@
+require 'csv'
+
+class MergeDuplicateAuths
+
+  def initialize
+    @logger = Logger.new(Rails.root.join('log', "merge_duplicate_auths.log"))
+    @auths_fixed = 0
+    @contribs_fixed = 0
+  end
+
+  # Change author ids of publications from dups to the master
+  # Destory duplicate auth rows
+  # Rebuild the publications
+  def merge(cap_id)
+    moved_contribs = false
+    auths = Author.where(:cap_profile_id => cap_id)
+    master = auths.shift
+
+    auths.each do |clone|
+      clone.contributions.each do |contrib|
+        unless(master.contributions.where(:publication_id => contrib.publication_id).exists?)
+          new_contrib = contrib.dup
+          new_contrib.author_id = master.id
+          new_contrib.save
+
+          @logger.info "Moved pub #{contrib.publication_id} to auth: #{master.id}"
+          @contribs_fixed += 1
+          moved_contribs = true
+        end
+      end
+
+      clone.destroy
+    end
+
+    master.reload if moved_contribs
+    master.publications.each do |pub|
+      pub.sync_publication_hash_and_db
+      pub.save
+    end
+
+  rescue ActiveRecord::RecordNotFound => e
+    @logger.warn "Author id not found #{auth_id}"
+  end
+
+  def work
+    ActiveRecord::Base.logger.level = 1
+    count = 0
+    dup_cap_ids = JSON.parse(IO.read(Rails.root.join('all_clones_cap_ids.json')))
+    dup_cap_ids.each do |cap_id|
+      begin
+        count += 1
+        @logger.info "Processed #{count}" if(count % 100 == 0)
+
+        merge cap_id
+      rescue => e
+        @logger.error "Problem with cap_id #{cap_id} #{e.inspect}"
+        @logger.error e.backtrace.join "\n"
+      end
+    end
+
+    @logger.info "Authors fixed: #{@auths_fixed}"
+    @logger.info "Contributions fixed: #{@contribs_fixed}"
+
+  rescue => e
+    @logger.error "#{e.inspect}"
+    @logger.error e.backtrace.join "\n"
+  end
+end
+
+c = MergeDuplicateAuths.new
+c.work
