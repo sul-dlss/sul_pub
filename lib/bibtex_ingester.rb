63ef60487ef728ff5321ceecd4c642ef5c9398c6
Willy Mene <wmene@stanford.edu>
2013-09-19 12:12:48 -0700

    Create allAuthors field in pub_hash for batch records

--- lib/bibtex_ingester.rb
+++ lib/bibtex_ingester.rb
@@ -6,18 +6,18 @@ class BibtexIngester
   @@book_type_mapping = ['book', 'booklet', 'inbook', 'incollection', 'manual', 'techreport']
   @@article_type_mapping = ['article', 'misc', 'unpublished']
   @@inproceedings_type_mapping = ['conference', 'proceedings', 'inproceedings']
-  
+
     def ingest_from_source_directory(directory)
       @batch_dir = directory || '/Users/jameschartrand/Documents/OSS/projects/stanford-cap/bibtex_import_files'
     #  puts "the dir is set #{@batch_dir}"
       @bibtex_import_logger = Logger.new(Rails.root.join('log', 'bibtext_import.log'))
-      @bibtex_import_logger.info "Started bibtext import #{DateTime.now}"   
+      @bibtex_import_logger.info "Started bibtext import #{DateTime.now}"
       @total_records_processed = 0
       @matches_on_issn_count = 0
       @matches_on_title_count = 0
       @total_duplicates = 0
       @total_faulty_record_count = 0
-      @total_successfully_ingested = 0 
+      @total_successfully_ingested = 0
       @missing_sunet_id_count = 0
       @records_without_sunet_id = 0
       @bad_file_error_count = 0
@@ -27,12 +27,12 @@ class BibtexIngester
       @batch_source_records_created_count = 0
       @unidentified_pub_type_count = 0
         Dir.open(@batch_dir).each do | batch_dir_name |
-          next if batch_dir_name == '.' or batch_dir_name == '..' 
+          next if batch_dir_name == '.' or batch_dir_name == '..'
           batch_dir_full_path = "#{@batch_dir}/#{batch_dir_name}"
           if File.directory? batch_dir_full_path
             Dir.open(batch_dir_full_path).each do | bibtex_file_name |
              # sunet_id = File.basename(bibtex_file_name, '.*')
-              next if batch_dir_name == '.' or batch_dir_name == '..' 
+              next if batch_dir_name == '.' or batch_dir_name == '..'
 
               file_full_path = "#{batch_dir_full_path}/#{bibtex_file_name}"
               log_file_full_path = "#{batch_dir_full_path}/#{bibtex_file_name}_import.log"
@@ -42,52 +42,52 @@ class BibtexIngester
               @ingested_for_file = 0
 
               @bibtex_file_logger = Logger.new(log_file_full_path)
-              @bibtex_file_logger.info "Started bibtext import for file #{DateTime.now}"  
-               
+              @bibtex_file_logger.info "Started bibtext import for file #{DateTime.now}"
+
               process_bibtex_file(file_full_path, batch_dir_name, bibtex_file_name)
-            
-              @bibtex_file_logger.info "Ended bibtext import for file #{DateTime.now}" 
+
+              @bibtex_file_logger.info "Ended bibtext import for file #{DateTime.now}"
               @bibtex_file_logger.info "#{@record_count_for_file} records processed."
               @bibtex_file_logger.info "#{@ingested_for_file} records were successfully ingested."
               @bibtex_file_logger.info "#{@errors_for_file} records had problems and weren't ingested."
            #   puts "#{@count_for_file} records processed for file: #{bibtex_file_name}"
-              
+
             end
-          end  
+          end
         end
-        @bibtex_import_logger.info "Finished bibtex import #{DateTime.now}"  
-        @bibtex_import_logger.info "#{@bad_file_error_count} files couldn't be parsed at all." 
-        @bibtex_import_logger.info "#{@good_file_count} files were parsed." 
-        @bibtex_import_logger.info "#{@missing_sunet_id_count} files containing #{@records_without_sunet_id} records weren't parsed because the sunet id wasn't found in the db." 
-        
+        @bibtex_import_logger.info "Finished bibtex import #{DateTime.now}"
+        @bibtex_import_logger.info "#{@bad_file_error_count} files couldn't be parsed at all."
+        @bibtex_import_logger.info "#{@good_file_count} files were parsed."
+        @bibtex_import_logger.info "#{@missing_sunet_id_count} files containing #{@records_without_sunet_id} records weren't parsed because the sunet id wasn't found in the db."
 
-        
-        @bibtex_import_logger.info "#{@total_records_processed} records processed for import." 
-        @bibtex_import_logger.info "#{@total_successfully_ingested} records were ingested or were duplicates." 
-        @bibtex_import_logger.info "#{@total_faulty_record_count} records weren't ingested or weren't duplicates." 
-        @bibtex_import_logger.info "#{@unidentified_pub_type_count} records weren't ingested because their pub type wasn't identified." 
-        
-        @bibtex_import_logger.info "#{@batch_source_records_created_count} batch_source_records were created."          
-        @bibtex_import_logger.info "#{@total_duplicates} were duplicates from prior batch." 
 
-        @bibtex_import_logger.info "#{@matches_on_issn_count} matched existing sw or pubmed pubs by issn." 
-        @bibtex_import_logger.info "#{@matches_on_title_count} matched existing existing sw or pubmed pubs by title." 
-        @bibtex_import_logger.info "#{@total_deduped_count} total records were deduplicated against sw or pubmed pubs." 
-        @bibtex_import_logger.info "#{@total_new_pubs} new publication records were created." 
-        
+
+        @bibtex_import_logger.info "#{@total_records_processed} records processed for import."
+        @bibtex_import_logger.info "#{@total_successfully_ingested} records were ingested or were duplicates."
+        @bibtex_import_logger.info "#{@total_faulty_record_count} records weren't ingested or weren't duplicates."
+        @bibtex_import_logger.info "#{@unidentified_pub_type_count} records weren't ingested because their pub type wasn't identified."
+
+        @bibtex_import_logger.info "#{@batch_source_records_created_count} batch_source_records were created."
+        @bibtex_import_logger.info "#{@total_duplicates} were duplicates from prior batch."
+
+        @bibtex_import_logger.info "#{@matches_on_issn_count} matched existing sw or pubmed pubs by issn."
+        @bibtex_import_logger.info "#{@matches_on_title_count} matched existing existing sw or pubmed pubs by title."
+        @bibtex_import_logger.info "#{@total_deduped_count} total records were deduplicated against sw or pubmed pubs."
+        @bibtex_import_logger.info "#{@total_new_pubs} new publication records were created."
+
       #  puts "#{@total_records_processed} records processed for import."
     end
 
     def process_bibtex_file(file_full_path, batch_name, bibtex_file_name)
 
         sunet_id = File.basename(bibtex_file_name, '.*')
-        
+
         author = Author.where(sunetid: sunet_id).first
-        if author.nil? 
+        if author.nil?
           puts "no author found for sunetid: #{sunet_id}"
           @missing_sunet_id_count += 1
-          @bibtex_import_logger.info "Couldn't find an author for sunetid: #{sunet_id}" 
-          @bibtex_file_logger.info "Couldn't find an author for sunetid: #{sunet_id}"  
+          @bibtex_import_logger.info "Couldn't find an author for sunetid: #{sunet_id}"
+          @bibtex_file_logger.info "Couldn't find an author for sunetid: #{sunet_id}"
            begin
             @records_without_sunet_id += BibTeX.open(file_full_path).count
 
@@ -98,17 +98,17 @@ class BibtexIngester
             @bibtex_file_logger.error e.message
             @bibtex_file_logger.error e.backtrace
           end
-        else 
+        else
           begin
             records = BibTeX.open(file_full_path)
           rescue => e
             puts "some error in the rescue"
             @bibtex_import_logger.error "Couldn't open the bibtex file, #{bibtex_file_name}, at all: "
-            @bibtex_import_logger.error "See the log file for #{sunet_id} for details." 
+            @bibtex_import_logger.error "See the log file for #{sunet_id} for details."
             @bibtex_import_logger.error e.message
             @bibtex_file_logger.error "Couldn't open the bibtex file at all: "
             @bibtex_file_logger.error e.message
-            @bibtex_file_logger.error e.backtrace  
+            @bibtex_file_logger.error e.backtrace
             @bad_file_error_count += 1
           end
           if records && ! records.empty?
@@ -116,7 +116,7 @@ class BibtexIngester
             records.each do | record |
                 @record_count_for_file += 1
                 @total_records_processed += 1
-                process_record(record, author)         
+                process_record(record, author)
             end
           else
             "puts no records for file: #{bibtex_file_name}"
@@ -127,41 +127,41 @@ class BibtexIngester
 
 def process_record(record, author)
   pub = nil
-  
+
   source_attrib_hash = {
       is_active: true,
-      sunet_id: author.sunetid, 
+      sunet_id: author.sunetid,
       successful_import: true,
       bibtex_source_data: record.to_s
   }
-  unless record["title"].blank? then source_attrib_hash[:title] =  record.title.to_s end 
-  unless record["year"].blank? then source_attrib_hash[:year] =  record.year.to_s end 
+  unless record["title"].blank? then source_attrib_hash[:title] =  record.title.to_s end
+  unless record["year"].blank? then source_attrib_hash[:year] =  record.year.to_s end
 
   begin
     existing_source_record = BatchUploadedSourceRecord.where(sunet_id: author.sunetid, title: record.title.to_s).first
-     
+
   rescue => e
-    @bibtex_import_logger.info "Search for existing batch upload for : #{record.to_s} failed probably because of unicode issue." 
+    @bibtex_import_logger.info "Search for existing batch upload for : #{record.to_s} failed probably because of unicode issue."
     @bibtex_import_logger.info "Error: #{e.message}"
   end
-  if ! existing_source_record.nil?   
+  if ! existing_source_record.nil?
       pub = existing_source_record.publication
       @total_duplicates +=1
       # if the publication has been updated with a sw or pubmed record since it was first submitted, then do nothing
       if (pub.sciencewire_id.blank?) && (pub.pmid.blank?)
-        pub.update_attributes(active: true, pub_hash: convert_bibtex_record_to_pub_hash(record, author)) 
-        existing_source_record.update_attributes(source_attrib_hash) 
+        pub.update_attributes(active: true, pub_hash: convert_bibtex_record_to_pub_hash(record, author))
+        existing_source_record.update_attributes(source_attrib_hash)
         existing_source_record.save
       end
   elsif ! determine_sul_pub_type(record.type.to_s.strip).nil?
     pub = find_existing_pub(record)
     if pub.nil?
-      
+
 
       pub = Publication.create(active: true, pub_hash: convert_bibtex_record_to_pub_hash(record, author))
       @total_new_pubs += 1
     #  puts pub.to_yaml
-    #  PublicationIdentifier.where( 
+    #  PublicationIdentifier.where(
     #                      :publication_id => pub.id,
     #                      :identifier_type => 'SULPubId').
     #            first_or_create(
@@ -170,23 +170,23 @@ def process_record(record, author)
     #                      :identifier_value => pub.id,
     #                      :identifier_uri => "http://sulcap.stanford.edu/publications/#{pub.id}")
     end
-    BatchUploadedSourceRecord.create(publication_id: pub.id).update_attributes(source_attrib_hash) 
+    BatchUploadedSourceRecord.create(publication_id: pub.id).update_attributes(source_attrib_hash)
     @batch_source_records_created_count += 1
     # create the contribution regardless of whether we created a new pub or are using an existing pub
     Contribution.where(
-                  author_id: author.id, 
+                  author_id: author.id,
                   publication_id: pub.id).
                 first_or_create(
                   cap_profile_id: author.cap_profile_id,
                   status: "approved",
                   visibility: "private",
                   featured: false)
-    #have to sync the pub hash to update new information, including new authorship        
-    pub.sync_publication_hash_and_db 
+    #have to sync the pub hash to update new information, including new authorship
+    pub.sync_publication_hash_and_db
     pub.save
   else
     @unidentified_pub_type_count += 1
-    @bibtex_import_logger.info "No pub type for: #{record.to_s}" 
+    @bibtex_import_logger.info "No pub type for: #{record.to_s}"
   end
   @ingested_for_file += 1
   @total_successfully_ingested += 1
@@ -194,8 +194,8 @@ def process_record(record, author)
 rescue => e
   #puts e.message
   #puts e.backtrace
-  @bibtex_import_logger.info "Record not ingested: #{record.to_s}" 
-  @bibtex_import_logger.info "Error: #{e.message}" 
+  @bibtex_import_logger.info "Record not ingested: #{record.to_s}"
+  @bibtex_import_logger.info "Error: #{e.message}"
   @total_faulty_record_count += 1
 end
 
@@ -209,27 +209,27 @@ def find_existing_pub(record)
 
   if !issn.blank? && !pages.blank? && !year.blank?
     begin
-      pub = Publication.where("(sciencewire_id is not null OR pmid is not null) 
+      pub = Publication.where("(sciencewire_id is not null OR pmid is not null)
             AND issn =? AND pages=? AND year=? ", issn, pages, year).first
     rescue => e
-      @bibtex_import_logger.info "Search for existing sw or pubmed pub for : #{record.to_s} failed probably because of unicode issue." 
+      @bibtex_import_logger.info "Search for existing sw or pubmed pub for : #{record.to_s} failed probably because of unicode issue."
       @bibtex_import_logger.info "Error: #{e.message}"
-    end   
-    if pub 
-      @matches_on_issn_count += 1  
+    end
+    if pub
+      @matches_on_issn_count += 1
     end
   end
   if pub.nil? && !title.blank? && !pages.blank? && !year.blank?
     begin
-      pub = Publication.where("(sciencewire_id is not null OR pmid is not null) 
+      pub = Publication.where("(sciencewire_id is not null OR pmid is not null)
             AND title= ? AND year= ? AND pages= ? ", title, year, pages).first
     rescue => e
-      @bibtex_import_logger.info "Search for existing sw or pubmed pub for : #{record.to_s} failed probably because of unicode issue." 
+      @bibtex_import_logger.info "Search for existing sw or pubmed pub for : #{record.to_s} failed probably because of unicode issue."
       @bibtex_import_logger.info "Error: #{e.message}"
     end
-    
-    if pub 
-      @matches_on_title_count += 1  
+
+    if pub
+      @matches_on_title_count += 1
     end
   end
   if pub then @total_deduped_count += 1 end
@@ -247,21 +247,21 @@ def convert_bibtex_record_to_pub_hash(record, author)
     isbn = record["isbn"].to_s.strip unless record["isbn"].blank?
     doi = record["doi"].to_s.strip unless record["doi"].blank?
 
-    unless issn.blank?  
-      issn_for_id_array = {:type => 'issn', :id => issn, :url => 'http://searchworks.stanford.edu/?search_field=advanced&number=' + issn} 
+    unless issn.blank?
+      issn_for_id_array = {:type => 'issn', :id => issn, :url => 'http://searchworks.stanford.edu/?search_field=advanced&number=' + issn}
       record_as_hash[:issn] = issn
     end
-    unless isbn.blank?  
-      isbn_for_id_array = {:type => 'isbn', :id => isbn, :url => 'http://searchworks.stanford.edu/?search_field=advanced&number=' + isbn} 
+    unless isbn.blank?
+      isbn_for_id_array = {:type => 'isbn', :id => isbn, :url => 'http://searchworks.stanford.edu/?search_field=advanced&number=' + isbn}
       record_as_hash[:isbn] = isbn
       identifiers << isbn_for_id_array
     end
-    unless doi.blank?  
+    unless doi.blank?
       doi_for_id_array = {:type => 'doi', :id => doi, :url => 'http://dx.doi.org/' + doi}
       identifiers << doi_for_id_array
       record_as_hash[:doi] = doi
     end
-    
+
 
     authorship_hash = {
       sul_author_id: author.id,
@@ -269,17 +269,18 @@ def convert_bibtex_record_to_pub_hash(record, author)
       visibility: 'public',
       featured: false}
     unless author.cap_profile_id.blank?  then authorship_hash[:cap_profile_id] = author.cap_profile_id end
-    
+
     record_as_hash[:authorship] = [ authorship_hash ]
-      
+
     record_as_hash[:provenance] = Settings.batch_source
     unless record["title"].blank? then record_as_hash[:title] = record.title.to_s.strip end
     #unless !record["title"].blank && record["title"].blank? then record_as_hash[:title] = record.chapter.to_s.strip end
     unless record["booktitle"].blank? then record_as_hash[:booktitle] = record.booktitle.to_s.strip end
-    unless record["author"].blank? 
-     record_as_hash[:author] = record.author.collect { |author| {:name => author.to_s} } 
-  end
-        
+    unless record["author"].blank?
+     record_as_hash[:author] = record.author.collect { |author| {:name => author.to_s} }
+     record_as_hash[:allAuthors] = record.author.to_a.join(', ')
+    end
+
     unless record["editor"].blank? then record_as_hash[:editor] = record.editor.to_s.strip end
     unless record["publisher"].blank? then record_as_hash[:publisher] =  record.publisher.to_s.strip end
     unless record["year"].blank? then record_as_hash[:year] = record.year.to_s.strip end
@@ -298,7 +299,7 @@ def convert_bibtex_record_to_pub_hash(record, author)
     end
 
     if sul_document_type == Settings.sul_doc_types.article || ! record.journal.blank?
-      journal_hash = {}   
+      journal_hash = {}
       unless record["journal"].blank? then journal_hash[:name] = record.journal.to_s.strip end
       unless record["volume"].blank? then journal_hash[:volume] = record.volume.to_s.strip end
       unless record["issue"].blank? then journal_hash[:issue] = record.issue.to_s.strip end
@@ -307,15 +308,15 @@ def convert_bibtex_record_to_pub_hash(record, author)
       unless record["month"].blank? then journal_hash[:month] = record.month.to_s.strip end
       journal_identifiers = Array.new
       unless issn.blank? then journal_identifiers << issn_for_id_array end
-        
+
     journal_hash[:identifier] = journal_identifiers
     unless journal_hash.empty? then record_as_hash[:journal] = journal_hash end
-    elsif 
+    elsif
        # if this is an article then the pages go in the article object, but if not put it in the main object.
       unless record["pages"].blank? then record_as_hash[:pages] = record.pages.to_s.strip  end
     end
-    
-    unless ! record["series"] 
+
+    unless ! record["series"]
         book_series_hash = {}
         book_series_hash[:identifier] = [issn_for_id_array]
         unless record["series"].blank? then book_series_hash[:title] = record.series.to_s.strip  end
@@ -330,7 +331,7 @@ def convert_bibtex_record_to_pub_hash(record, author)
 end
 
 def determine_sul_pub_type(bibtex_type)
-  if @@book_type_mapping.include?(bibtex_type) 
+  if @@book_type_mapping.include?(bibtex_type)
     Settings.sul_doc_types.book
   elsif @@article_type_mapping.include?(bibtex_type)
     Settings.sul_doc_types.article
--- spec/lib/bibtex_ingester_spec.rb
+++ spec/lib/bibtex_ingester_spec.rb
@@ -1,55 +1,55 @@
 require 'spec_helper'
 
 describe BibtexIngester do
-	let!(:author_with_bibtex) { create :author, sunetid: "james", cap_profile_id: 333333 }	
+	let!(:author_with_bibtex) { create :author, sunetid: "james", cap_profile_id: 333333 }
 	let!(:bibtex_ingester) {BibtexIngester.new}
-		
+
 	#	puts author_with_bibtex.to_s
 
 	describe "#harvest_from_directory_of_bibtex_files" do
 
-	  it "creates pub for each new bibtex record" do        
+	  it "creates pub for each new bibtex record" do
         expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-						}.to change(Publication, :count).by(4)     
-		expect(author_with_bibtex.publications).to have(4).items 
+						}.to change(Publication, :count).by(4)
+		expect(author_with_bibtex.publications).to have(4).items
 	  end
 
-	  it "associates pubs with author by sunet" do        
-        	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)					   
-			expect(author_with_bibtex.publications).to have(4).items 
+	  it "associates pubs with author by sunet" do
+        	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+			expect(author_with_bibtex.publications).to have(4).items
 	  end
 
 	  it "doesn't duplicate existing publications" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to_not change(Publication, :count)
-		
+
 	  end
 	  it "doesn't duplicate existing publication identifiers" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-	  
+
 		expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to_not change(PublicationIdentifier, :count)
-		
+
 	  end
 	  it "doesn't duplicate existing contributions" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-	  	
+
 		expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to_not change(Contribution, :count)
 	  end
 	  it "creates new publication identifiers" do
-	  	
+
 	  	expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(PublicationIdentifier, :count).by(2)
 	  end
 	  it "creates new contributions" do
-	  	
+
 	  	expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(Contribution, :count).by(4)
 	  end
 
-	  it "creates BatchUploadedSourceRecord" do 	
+	  it "creates BatchUploadedSourceRecord" do
 	  	expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(BatchUploadedSourceRecord, :count).by(4)
 	  end
@@ -65,21 +65,21 @@ describe BibtexIngester do
 	  	expect(pub.pub_hash[:mla_citation].downcase).to include(pub.title.downcase)
 	  end
 
-	  
+
 	  it "puts issn into journal part of pubhash" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	pub = Publication.where(title: 'Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly').first
 
 	  	expect(pub.pub_hash[:journal][:identifier].select {|k| k[:type] == 'issn'}.first[:id]).to include('5555')
-	  	
+
 	  end
 
-	  
+
 	  it "adds publication_identifier for isbn" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	expect(PublicationIdentifier.exists?(identifier_type: 'isbn', identifier_value: 3233333)).to be_true
 	  end
-	  
+
 	  it "adds publication_identifier for DOI" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	expect(PublicationIdentifier.exists?(identifier_type: 'doi', identifier_value: 8484848484)).to be_true
@@ -95,26 +95,32 @@ describe BibtexIngester do
 	it "puts SULPubId into identifer part of pubhash" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	pub = Publication.where(title: 'Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly').first
-	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'SULPubId'}.first[:id]).to match(pub.id.to_s)	  	
+	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'SULPubId'}.first[:id]).to match(pub.id.to_s)
 	  end
 
 it "puts DOI into identifer part of pubhash" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
-	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'doi'}.first[:id]).to match('8484848484')	  	
+	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'doi'}.first[:id]).to match('8484848484')
 	  end
 
 it "puts isbn into identifer part of pubhash" do
 	  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 	  	pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
-	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'isbn'}.first[:id]).to match('3233333')	  	
+	  	expect(pub.pub_hash[:identifier].select {|k| k[:type] == 'isbn'}.first[:id]).to match('3233333')
+	  end
+
+	  it "places all of the authors into the allAuthors field of the pub_hash" do
+	    bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+	  	pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
+	  	expect(pub.pub_hash[:allAuthors]).to eq 'Goldstein, Mary Kane, Miller, David E., Davies, Sheryl M., Garber, Alan M.'
 	  end
 
 
 	  context "when depuping by issn, year, pages" do
 	  	let!(:existing_matching_pub_issn) {create :publication, pmid: 3323434, pub_hash: {title: 'Quelque Titre', type: 'article', pmid: 3323434, year: 2002, pages: '295-299', issn: '234234', author: [{name: "Jackson, Joe"}], authorship:[{sul_author_id: 2222, status: "denied", visibility: "public", featured: true}]}}
 	  	let!(:contribution) {create :contribution, author: author_with_bibtex, publication: existing_matching_pub_issn}
-	
+
 
 		it "doesn't add duplicate contributions" do
 			#puts existing_matching_pub_issn.to_yaml
@@ -135,7 +141,7 @@ it "puts isbn into identifer part of pubhash" do
 	  	let!(:pub_with_same_title) {create :publication, pmid: 332423434, pub_hash: {title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living', pmid: 3323434, type: 'article', year: 2002, pages: '295-299', author: [{name: "Jackson, Joe"}], authorship:[{sul_author_id: 2222, status: "denied", visibility: "public", featured: true}]}}
 	  	let!(:contribution) {create :contribution, author: author_with_bibtex, publication: pub_with_same_title}
 		it "doesn't add duplicate contributions" do
-			
+
 			#puts pub_with_title.to_yaml
 	  		expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(Contribution, :count).by(3)
@@ -144,7 +150,7 @@ it "puts isbn into identifer part of pubhash" do
 	  		expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(PublicationIdentifier, :count).by(0)
 	  	end
-	  	it "doesn't duplicate existing publications" do  	
+	  	it "doesn't duplicate existing publications" do
 	  		expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 						}.to change(Publication, :count).by(3)
 		end
@@ -160,7 +166,7 @@ it "puts isbn into identifer part of pubhash" do
 		 #	}.to change { oldPub.pub_hash[:title] }.from("Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly ").
 		#		to("The new title")
 		 # end
-		
+
 		  it "doesn't duplicate identifiers" do
 		  	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
 		  	expect {bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
@@ -189,7 +195,7 @@ it "puts isbn into identifer part of pubhash" do
 		  	expect(oldPub.pub_hash[:issn]).to match("234235")
 		  	#oldPub = Publication.first
 		  #	puts "old pub: #{oldPub.to_yaml}"
-		  
+
 		  end
 
 		end
