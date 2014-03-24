3808eebd259a5f5a84d4da8b723f62fe4882fe48
Willy Mene <wmene@stanford.edu>
2014-03-24 14:36:59 -0700

    Adding script to find pub duplicates and issue report

---
+++ script/dup_report.rb
@@ -0,0 +1,75 @@
+
+require 'set'
+require 'csv'
+
+class Reporter
+
+  def initialize swids_file, pmids_file, wos_file
+    @swids = build_id_set swids_file
+    @pmids = build_id_set pmids_file
+    @wosids = build_id_set wos_file
+    @log = Logger.new('pub_dups.log')
+    @sw_pub_ids = []
+    @pmid_sw_pub_ids = []
+    @wos_pub_ids = []
+    @all_pub_ids = Set.new
+  end
+
+  def build_id_set filename
+    lines = IO.readlines(filename).map {|l| l.strip}.reject {|l| l.empty?}
+    Set.new(lines)
+  end
+
+  def work
+    @swids.each do |swid|
+      Publication.where(:sciencewire_id => swid).each do |pub|
+        if @pmids.delete? pub.pmid
+          @log.info "Removing pmid #{pub.pmid} from #{pub.id} with swid #{swid}"
+        end
+        @sw_pub_ids << [swid, pub.id]
+        @all_pub_ids.add pub.id
+      end
+    end
+
+    @pmids.each do |pmid|
+      Publication.where(:pmid => pmid).each do |pub|
+        if(@all_pub_ids.add? pub.id)
+          @pmid_sw_pub_ids << [pmid, pub.sciencewire_id, pub.id]
+        else
+          @log.info "Dup already found - pubid:#{pub.id} pmid:#{pmid}"
+        end
+      end
+    end
+
+    @wosids.each do |wosid|
+      ids = PublicationIdentifier.where(:identifier_type => 'WoSItemID', :identifier_value => wosid).pluck(:publication_id)
+      ids.each do |id|
+        if(@all_pub_ids.add? id)
+          @wos_pub_ids << [wosid, id]
+        else
+          @log.info "Dup already found - pubid:#{id} wosid:#{wosid}"
+        end
+      end
+    end
+
+    CSV.open('sw_pub_ids.csv', 'w') do |csv|
+      csv << ['sciencewire_id', 'sulpubid']
+      @sw_pub_ids.each {|a| csv << a}
+    end
+
+    CSV.open('pmid_sw_pub_ids.csv', 'w') do |csv|
+      csv << ['pubmedid', 'sciencewire_id', 'sulpubid']
+      @pmid_sw_pub_ids.each {|a| csv << a}
+    end
+
+    CSV.open('wos_pub_ids.csv', 'w') do |csv|
+      csv << ['wosid', 'sulpubid']
+      @wos_pub_ids.each {|a| csv << a}
+    end
+
+  end
+
+end
+
+r = Reporter.new('/Users/wmene/dev/cap/dup_sw_ids.txt', '/Users/wmene/dev/cap/dup_pmids.txt', '/Users/wmene/dev/cap/dup_wos.txt')
+r.work
