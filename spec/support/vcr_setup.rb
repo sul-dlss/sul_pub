4fe908c38e6980af5d1e47543480ae51d2de92b3
Chris Beer <cabeer@stanford.edu>
2015-06-29 10:00:34 -0700

    Don't re-record VCR cassettes automatically.

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -4,5 +4,5 @@ VCR.configure do |c|
  	c.cassette_library_dir = 'fixtures/vcr_cassettes'
   	c.hook_into :webmock
   	c.allow_http_connections_when_no_cassette = true
-  	c.default_cassette_options = { :record => :new_episodes , :re_record_interval => 7.days }
+    c.default_cassette_options = { :record => :new_episodes }
 end
