94011cf41a65e1a58082975e5af05a9366e09ed5
Laney McGlohon <laneymcg@stanford.edu>
2016-03-30 11:42:56 -0700

    Configured squash to use dlss common practice

--- config/initializers/squash.rb
+++ config/initializers/squash.rb
@@ -1,1 +1,5 @@
-Squash::Ruby.configure disabled: true
+Squash::Ruby.configure api_host: ConfigSettings.SQUASH.API_HOST,
+                       api_key: ConfigSettings.SQUASH.API_KEY,
+                       disabled: ConfigSettings.SQUASH.DISABLED,
+                       environment: ConfigSettings.SQUASH.ENVIRONMENT || Rails.env,
+                       revision_file: File.join(Rails.root, "REVISION")
