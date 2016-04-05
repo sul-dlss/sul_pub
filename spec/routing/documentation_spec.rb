20dd5ebd998f311c1b7a15bda3777893131356fd
Jack Reed <phillipjreed@gmail.com>
2016-04-04 18:56:19 -0700

    Add routing specs for HighVoltage documentation routes

---
+++ spec/routing/documentation_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe 'Documentation routing' do
+  it 'routes root path to api' do
+    expect(get: '/').to route_to(
+      id: 'api',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /api to api' do
+    expect(get: '/api').to route_to(
+      id: 'api',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /pubapi to pubapi' do
+    expect(get: '/pubapi').to route_to(
+      id: 'pubapi',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /pubsapi to pubsapi' do
+    expect(get: '/pubsapi').to route_to(
+      id: 'pubsapi',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /pollapi to pollapi' do
+    expect(get: '/pollapi').to route_to(
+      id: 'pollapi',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /authorshipapi to authorshipapi' do
+    expect(get: '/authorshipapi').to route_to(
+      id: 'authorshipapi',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+  it 'routes /queryapi to queryapi' do
+    expect(get: '/queryapi').to route_to(
+      id: 'queryapi',
+      controller: 'high_voltage/pages',
+      action: 'show'
+    )
+  end
+end
