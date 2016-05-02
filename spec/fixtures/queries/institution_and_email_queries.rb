117938e23a05bf748e0df84c4e639c3354df915e
Laney McGlohon <laneymcg@stanford.edu>
2016-05-02 11:33:55 -0700

    Added no institution and no email query test  - Added fixture data  - Added exclude spec/fixtures directory from rubocop

--- spec/fixtures/queries/institution_and_email_queries.rb
+++ spec/fixtures/queries/institution_and_email_queries.rb
@@ -154,4 +154,56 @@ module InstitutionEmailQueries
     ]]>
     XML
   end
+
+  def no_institution_no_email_provided
+    <<-XML
+    <![CDATA[
+      <query xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+        <Criterion>
+          <Criteria>
+            <Criterion>
+              <TextSearch>
+                <QueryPredicate>("brown,charlie" or "BROWN,C") and "stanford"</QueryPredicate>
+                <SearchType>ExactMatch</SearchType>
+                <Columns>AggregateText</Columns>
+                <MaximumRows>200</MaximumRows>
+              </TextSearch>
+            </Criterion>
+              <Criterion>
+                <Filter>
+                  <Column>AuthorLastName</Column>
+                  <Operator>BeginsWith</Operator>
+                  <Value>BROWN</Value>
+                </Filter>
+              </Criterion>
+              <Criterion>
+                <Filter>
+                  <Column>AuthorFirstName</Column>
+                  <Operator>BeginsWith</Operator>
+                  <Value>C</Value>
+                </Filter>
+              </Criterion>
+              <Criterion>
+                <Filter>
+                  <Column>DocumentCategory</Column>
+                  <Operator>In</Operator>
+                  <Values>
+                    <Value>Journal Document</Value>
+                    <Value>Conference Proceeding Document</Value>
+                  </Values>
+                </Filter>
+              </Criterion>
+          </Criteria>
+        </Criterion>
+        <Columns>
+          <SortColumn>
+            <Column>Rank</Column>
+            <Direction>Descending</Direction>
+          </SortColumn>
+        </Columns>
+        <MaximumRows>200</MaximumRows>
+      </query>
+    ]]>
+    XML
+  end
 end
