65164f547c37947846895462b06c0c5420ebddda
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2016-05-02 16:54:37 -0700

    Add ScienceWire::AuthorInstitution

---
+++ spec/lib/science_wire/author_institution_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe ScienceWire::AuthorInstitution do
+  describe '#initialize' do
+    subject { described_class.new(nil, nil) }
+    it 'casts name to String' do
+      expect(subject.name).to be_an String
+    end
+    it 'strips whitespace from name' do
+      name = described_class.new('  name  ').name
+      expect(name).to be_an String
+      expect(name).to eq('name')
+    end
+    it 'address is optional' do
+      expect{ described_class.new('name') }.not_to raise_error
+    end
+    it 'address defaults to an empty Hash' do
+      address = described_class.new('name').address
+      expect(address).to be_an Hash
+      expect(address).to be_empty
+    end
+  end
+  describe '#normalize_name' do
+    context 'when name has excluded words at the beginning of the string' do
+      subject { described_class.new('The University of North Carolina') }
+      it 'normalizes institution name' do
+        expect(subject.normalize_name).to eq 'north carolina'
+      end
+    end
+    context 'when present with removed words throughout the institution string' do
+      subject { described_class.new('The Flinders University of South Australia') }
+      it 'normalizes institution name' do
+        expect(subject.normalize_name).to eq 'flinders south australia'
+      end
+    end
+    context 'when not present' do
+      subject { described_class.new(nil) }
+      it 'returns empty string' do
+        expect(subject.normalize_name).to eq ''
+      end
+    end
+  end
+end
