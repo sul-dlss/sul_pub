737f678b37aebe4948f919b0d723e261a9d69980
Jack Reed <phillipjreed@gmail.com>
2016-05-06 09:56:35 -0400

    Creates AuthorHarvestJob
    Essentially does the same thing as our Rake task.

---
+++ app/jobs/author_harvest_job.rb
@@ -0,0 +1,19 @@
+class AuthorHarvestJob < ActiveJob::Base
+  queue_as :default
+
+  ##
+  # Performs an asynchronous harvest and save for a given Author
+  # @param [String] cap_profile_id
+  # @param [Boolean] harvest_alternate_names
+  def perform(cap_profile_id, harvest_alternate_names: false)
+    harvester = ScienceWireHarvester.new
+    harvester.use_author_identities = harvest_alternate_names
+    author = Author.where(cap_profile_id: cap_profile_id).first
+    author ||= Author.fetch_from_cap_and_create(cap_profile_id)
+    harvester.harvest_pubs_for_author_ids author.id
+    pubs = Contribution.where(author_id: author.id).map(&:publication).each do |p|
+      logger.info "publication #{p.id}: #{p.pub_hash[:apa_citation]}"
+    end
+    logger.info "Number of publications #{pubs.count}"
+  end
+end
