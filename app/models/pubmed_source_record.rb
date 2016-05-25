f5d09c0c727ee14306c16df1d7524b9f54db7611
Darren Hardy <drh@stanford.edu>
2016-05-25 11:58:29 -0700

    remove incorrect "Invalid pmids" log output

--- app/models/pubmed_source_record.rb
+++ app/models/pubmed_source_record.rb
@@ -55,7 +55,6 @@ class PubmedSourceRecord < ActiveRecord::Base
     # http.finish
     count = 0
     source_records = []
-    @cap_import_pmid_logger = Logger.new(Rails.root.join('log', 'cap_import_pmid.log'))
     Nokogiri::XML(the_incoming_xml).xpath('//PubmedArticle').each do |pub_doc|
       pmid = pub_doc.xpath('MedlineCitation/PMID').text
       begin
@@ -73,7 +72,6 @@ class PubmedSourceRecord < ActiveRecord::Base
       end
     end
     PubmedSourceRecord.import source_records
-    @cap_import_pmid_logger.info 'Invalid pmids: ' + pmids.to_a.join(',')
   end
 
   def extract_abstract_from_pubmed_record(pubmed_record)
