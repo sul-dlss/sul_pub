8e29ce5c86137e553a8d01815c9d150caee90470
Peter Mangiafico <peter@mangiafico.org>
2017-05-03 15:30:30 -0700

    rename deployment names to dlss standards

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,4 +2,6 @@ server 'sul-pub-dev.stanford.edu', user: fetch(:user), roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
+set :rails_env, 'development'
+
 set :bundle_without, %w(test).join(' ')
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,6 +2,8 @@ server 'sul-pub-prod.stanford.edu', user: fetch(:user), roles: %w(web db app har
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
+set :rails_env, 'production'
+
 set :bundle_without, %w(test development).join(' ')
 
 set :log_level, :info
