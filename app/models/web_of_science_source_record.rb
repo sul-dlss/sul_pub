116fa8b5058067164a81d6a265755cfedcc6348a
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-10-10 15:37:19 -0700

    WebOfScienceSourceRecord creation
    - initial migration
      - rails g model WebOfScienceSourceRecord database:string is_active:boolean source_data:text source_fingerprint:string uid:string
      - rake db:migrate
    
    - index on UID
      - bundle exec rake db:rollback STEP=1
      - add the index to the migration, then
      - bundle exec rake db:migrate
    
    - added fields and methods
      - #doc -> Nokogiri::XML::Document from source_data
      - #to_xml -> String from doc.to_xml
      - initialize callback to extract field data
      - validation for field data extracted
    
    - @atz suggestion to call `super || something` instead of init callback
    - revise is_active to active
    - git rm spec/factories/web_of_science_source_records.rb

---
+++ app/models/web_of_science_source_record.rb
@@ -0,0 +1,45 @@
+class WebOfScienceSourceRecord < ActiveRecord::Base
+
+  validates :active, :database, :source_data, :source_fingerprint, :uid, presence: true
+
+  after_initialize :check_source_data
+
+  # @return doc [Nokogiri::XML::Document] XML document
+  def doc
+    @doc ||= Nokogiri::XML(source_data)
+  end
+
+  def active
+    # assume records are active until we discover a deprecation attribute
+    super || self.active = true
+  end
+
+  def database
+    super || begin
+      uid_split = uid.split(':')
+      self.database = uid_split.length > 1 ? uid_split[0] : nil
+    end
+  end
+
+  def source_fingerprint
+    super || self.source_fingerprint = Digest::SHA2.hexdigest(source_data)
+  end
+
+  # @return xml [String] XML
+  def to_xml
+    doc.to_xml(save_with: XML_OPTIONS).strip
+  end
+
+  def uid
+    super || self.uid = doc.search('UID').text
+  end
+
+  private
+
+    XML_OPTIONS = Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION
+
+    def check_source_data
+      raise 'Missing source_data' if source_data.nil?
+    end
+
+end
