cf82bf888c2b57cf1f87df4a358babddb91fbf2c
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-11-02 15:50:29 -0700

    Revert "Publication model has methods that should be private"

--- spec/api/sul_bib/sourcelookup_spec.rb
+++ spec/api/sul_bib/sourcelookup_spec.rb
@@ -66,7 +66,7 @@ describe SulBib::API, :vcr do
             }
           ]
         }
-        publication.send(:sync_identifiers_in_pub_hash_to_db)
+        publication.sync_identifiers_in_pub_hash_to_db
         result = sourcelookup_by_doi
         expect(result['metadata']).to include('records')
         expect(result['metadata']['records']).to eq('1')
