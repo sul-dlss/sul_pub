b5acc6edde25eea17f45ddcdc43c4d3530855d61
Joe Atzberger <ohiocore@gmail.com>
2017-11-02 12:08:56 -0700

    3 PubmedSourceRecord class methods can be private
    First savings from cap_cutover purge, these aren't called anywhere else.
    
    Tests converted to use `.send` as normal.

--- spec/models/pubmed_source_record_spec.rb
+++ spec/models/pubmed_source_record_spec.rb
@@ -1,4 +1,3 @@
-
 describe PubmedSourceRecord, :vcr do
   let(:pmid_created_1999) { 10_000_166 }
 
@@ -130,15 +129,15 @@ describe PubmedSourceRecord, :vcr do
 
   describe '.get_pubmed_record_from_pubmed' do
     it 'returns an instance of PubmedSourceRecord' do
-      record = described_class.get_pubmed_record_from_pubmed(pmid_created_1999)
+      record = described_class.send(:get_pubmed_record_from_pubmed, pmid_created_1999)
       expect(record).to be_an described_class
     end
     it 'calls PubmedSourceRecord.get_and_store_records_from_pubmed' do
       expect(described_class).to receive(:get_and_store_records_from_pubmed)
-      described_class.get_pubmed_record_from_pubmed(pmid_created_1999)
+      described_class.send(:get_pubmed_record_from_pubmed, pmid_created_1999)
     end
     it 'extracts fields - pmid' do
-      record = described_class.get_pubmed_record_from_pubmed(pmid_created_1999)
+      record = described_class.send(:get_pubmed_record_from_pubmed, pmid_created_1999)
       expect(record.pmid).to eq pmid_created_1999
     end
   end
@@ -169,7 +168,7 @@ describe PubmedSourceRecord, :vcr do
   context '.source_as_hash' do
     context 'DOI extraction' do
       def doi(pmid)
-        record = described_class.get_pubmed_record_from_pubmed(pmid)
+        record = described_class.send(:get_pubmed_record_from_pubmed, pmid)
         return nil if record.nil?
         record.source_as_hash[:identifier].find { |id| id[:type] == 'doi' }
       end
