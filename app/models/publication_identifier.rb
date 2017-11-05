06ce59173656ba5697566c3269c8687f8b0268af
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-11-05 15:03:53 -0800

    PublicationIdentifier#update_pub_hash can update or delete identifier
    - pub_hash_update cannot save the publication
    - must avoid callback hell
    - specs check that publication.save callbacks do not change mocks

--- app/models/publication_identifier.rb
+++ app/models/publication_identifier.rb
@@ -10,4 +10,24 @@ class PublicationIdentifier < ActiveRecord::Base
     ident_hash[:url] = identifier_uri if identifier_uri.present?
     ident_hash
   end
+
+  # Update the identifier_type entries in publication.pub_hash[:identifier];
+  # the publication.pub_hash[:identifier] is not persisted by this method, to
+  # avoid complex recurrent call stacks in the rails callback stack.
+  # @param delete [Boolean] delete this identifier from the pub_hash[:identifier] (default: false)
+  def pub_hash_update(delete: false)
+    publication.pub_hash[:identifier] = pub_hash_reject
+    publication.pub_hash[:identifier] << identifier unless delete
+  end
+
+  private
+
+    # Reject all pub_id.identifier_type entries from pub_id.publication.pub_hash[:identifier]
+    # - the pub_hash[:identifier] is not modified by this method
+    # @return [Hash]
+    def pub_hash_reject
+      pub_ids = publication.pub_hash[:identifier] || []
+      pub_ids.reject { |id| id[:type] == identifier_type }
+    end
+
 end
