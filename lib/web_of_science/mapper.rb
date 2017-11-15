3a3c2deddea4473ab1c7e35afd6cd55af742f724
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-11-15 13:39:59 -0800

    WebOfScience::Mapper - base class for record data mappers

---
+++ lib/web_of_science/mapper.rb
@@ -0,0 +1,27 @@
+module WebOfScience
+
+  # Map WOS record data into the SUL PubHash data
+  class Mapper
+
+    # @param rec [WebOfScience::Record]
+    def initialize(rec)
+      raise(ArgumentError, 'rec must be a WebOfScience::Record') unless rec.is_a? WebOfScience::Record
+      @rec = rec
+    end
+
+    # @return [Hash]
+    def pub_hash
+      mapper
+    end
+
+    private
+
+      attr_reader :rec
+
+      def mapper
+        # subclasses can map data, otherwise this method should never get called
+        raise(NotImplementedError, 'Move along, this is not the mapper your looking for.')
+      end
+
+  end
+end
