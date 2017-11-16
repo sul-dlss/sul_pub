b78773acf94185626d13d691aef4216586cf8d89
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-11-16 11:04:28 -0800

    WebOfScienceSourceRecord - migration to add DOI and PMID fields

---
+++ db/migrate/20171116185807_web_of_science_source_record_ids.rb
@@ -0,0 +1,9 @@
+class WebOfScienceSourceRecordIds < ActiveRecord::Migration
+  def change
+    add_column :web_of_science_source_records, :doi, :string
+    add_column :web_of_science_source_records, :pmid, :string
+
+    add_index :web_of_science_source_records, :doi, name: 'web_of_science_doi_index'
+    add_index :web_of_science_source_records, :pmid, name: 'web_of_science_pmid_index'
+  end
+end
