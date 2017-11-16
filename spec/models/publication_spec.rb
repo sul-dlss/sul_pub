ed6d7e48936d3791abc9f25246f68e18de09c7fe
Joe Atzberger <ohiocore@gmail.com>
2017-11-15 18:59:39 -0800

    Use associations, better hygiene around saving, including dependents
    Clarified responsibilities and timing around saving, especially for
    methods in the callback chain.
    
    The `.save!` in the API are useful because the `.reload` in the next
    line is going to fail anyway if the save didn't succeed!
    
    In `Publication`, the main associations have `autosave: true`,
    which means we shouldn't take separate trips to the DB
    to save dependent objects, but instead tie their persistence to the
    `.save` call on the `Publication` object.  See:
    http://api.rubyonrails.org/classes/ActiveRecord/AutosaveAssociation.html
    
    Consolidated some of the private sync functionality in Publication.
    
    Use ActiveRecord associations to build/initialize/delete dependent
    objects.

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -101,10 +101,11 @@ describe Publication do
     end
   end
 
-  describe 'sync_identifiers_in_pub_hash_to_db' do
+  describe 'sync_identifiers_in_pub_hash' do
     it 'should sync identifiers in the pub hash to the database' do
       publication.pub_hash = { identifier: [{ type: 'x', id: 'y', url: 'z' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       i = PublicationIdentifier.last
       expect(i.identifier_type).to eq('x')
       expect(publication.publication_identifiers.reload).to include(i)
@@ -113,36 +114,44 @@ describe Publication do
     it 'should not persist SULPubIds' do
       publication.pub_hash = { identifier: [{ type: 'SULPubId', id: 'y', url: 'z' }] }
       expect do
-        publication.send(:sync_identifiers_in_pub_hash_to_db)
+        publication.send(:sync_identifiers_in_pub_hash)
+        publication.save
       end.to_not change(publication, :publication_identifiers)
     end
 
     it 'updates existing ids with new values' do
       publication.pub_hash = { identifier: [{ type: 'x', id: 'y', url: 'z' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       publication.pub_hash = { identifier: [{ type: 'x', id: 'y2', url: 'z2' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       ids = PublicationIdentifier.where(publication_id: publication.id).all
-      expect(ids.size).to eq(1)
+      expect(ids).not_to be_empty
       expect(ids.first.identifier_type).to eq('x')
       expect(ids.first.identifier_value).to eq('y2')
       expect(ids.first.identifier_uri).to eq('z2')
+      expect(ids.size).to eq(1)
     end
 
     it 'deletes ids from the database that are not longer in the pub_hash' do
       publication.pub_hash = { identifier: [{ type: 'x', id: 'y', url: 'z' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       publication.pub_hash = { identifier: [{ type: 'a', id: 'b', url: 'c' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       expect(PublicationIdentifier.where(publication_id: publication.id, identifier_type: 'x').count).to eq(0)
       expect(PublicationIdentifier.where(publication_id: publication.id, identifier_type: 'a').count).to eq(1)
     end
 
     it 'does not delete legacy_cap_pub_id when missing from the incoming pub_hash' do
       publication.pub_hash = { identifier: [{ type: 'legacy_cap_pub_id', id: '258214' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       publication.pub_hash = { identifier: [{ type: 'another', id: 'id', url: 'with a url' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       expect(PublicationIdentifier.where(publication_id: publication.id, identifier_type: 'legacy_cap_pub_id').count).to eq(1)
       expect(PublicationIdentifier.where(publication_id: publication.id, identifier_type: 'another').count).to eq(1)
     end
@@ -376,7 +385,8 @@ describe Publication do
   describe '.find_by_doi' do
     it 'returns one Publication that has this doi' do
       publication.pub_hash = { identifier: [{ type: 'doi', id: '10.1016/j.mcn.2012.03.008', url: 'https://dx.doi.org/10.1016/j.mcn.2012.03.008' }] }
-      publication.send(:sync_identifiers_in_pub_hash_to_db)
+      publication.send(:sync_identifiers_in_pub_hash)
+      publication.save
       res = Publication.find_by_doi('10.1016/j.mcn.2012.03.008')
       expect(res.id).to eq(publication.id)
     end
