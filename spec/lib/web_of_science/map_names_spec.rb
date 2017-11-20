65d64baf197db8ffa40c23e99c3802264cb0578f
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-11-20 10:54:58 -0800

    WebOfScience::MapNames - responsible for mapping names into pub_hash

---
+++ spec/lib/web_of_science/map_names_spec.rb
@@ -0,0 +1,57 @@
+describe WebOfScience::MapNames do
+  let(:wos_encoded_xml) { File.read('spec/fixtures/wos_client/wos_encoded_record.html') }
+  let(:wos_record) { WebOfScience::Record.new(encoded_record: wos_encoded_xml) }
+
+  let(:medline_encoded_xml) { File.read('spec/fixtures/wos_client/medline_encoded_record.html') }
+  let(:medline_record) { WebOfScience::Record.new(encoded_record: medline_encoded_xml) }
+
+  describe '#new' do
+    it 'works with WOS records' do
+      result = described_class.new(wos_record)
+      expect(result).to be_an described_class
+    end
+    it 'raises ArgumentError with nil params' do
+      expect { described_class.new }.to raise_error(ArgumentError)
+    end
+    it 'raises ArgumentError with anything other than WebOfScience::Record' do
+      expect { described_class.new('could be xml') }.to raise_error(ArgumentError)
+    end
+  end
+
+  shared_examples 'pub_hash' do
+    it 'works' do
+      expect(pub_hash).to be_an Hash
+    end
+  end
+
+  shared_examples 'contains_author_data' do
+    it 'has an authors' do
+      expect(pub_hash[:author]).not_to be_nil
+    end
+    it 'has an authorcount' do
+      expect(pub_hash[:authorcount]).not_to be_nil
+    end
+  end
+
+  context 'WOS records' do
+    let(:pub_hash_class) { described_class.new(wos_record) }
+    let(:pub_hash) { pub_hash_class.pub_hash }
+
+    it 'works with WOS records' do
+      expect(pub_hash_class).to be_an described_class
+    end
+    it_behaves_like 'pub_hash'
+    it_behaves_like 'contains_author_data'
+  end
+
+  context 'MEDLINE records' do
+    let(:pub_hash_class) { described_class.new(medline_record) }
+    let(:pub_hash) { pub_hash_class.pub_hash }
+
+    it 'works with MEDLINE records' do
+      expect(pub_hash_class).to be_an described_class
+    end
+    it_behaves_like 'pub_hash'
+    it_behaves_like 'contains_author_data'
+  end
+end
