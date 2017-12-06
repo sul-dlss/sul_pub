87ab7749b6fe8dfff7bf186f4a91c45251d54875
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-05 16:08:06 -0800

    Csl::Citation - extract spec fixtures for pub_hash case study data

--- spec/lib/csl/citation_spec.rb
+++ spec/lib/csl/citation_spec.rb
@@ -1,6 +1,7 @@
 
 describe Csl::Citation do
   # Fixture data from spec/fixtures/pub_hash/*.rb
+  include PubHash::CaseStudy
   include PubHash::Conference
   include PubHash::WorkingPaper
 
@@ -142,46 +143,6 @@ describe Csl::Citation do
     h[:publicationUrl] = ''
     h.delete(:pages)
     h
-  end
-
-  let(:case_study_pub_hash) do
-    {
-      title: 'HCL Technologies',
-      type: 'caseStudy',
-      provenance: 'CAP',
-      author: [
-        {
-          name: 'Hill  Linda',
-          lastname: 'Hill',
-          firstname: 'Linda',
-          middlename: '',
-          alternate: [],
-          role: 'author',
-          additionalProperties: {}
-        },
-        {
-          name: 'Khanna  Tarun',
-          lastname: 'Khanna',
-          firstname: 'Tarun',
-          middlename: '',
-          alternate: [],
-          role: 'author',
-          additionalProperties: {}
-        },
-        {
-          name: 'Stecker A Emily',
-          lastname: 'Stecker',
-          firstname: 'Emily',
-          middlename: 'A',
-          alternate: [],
-          role: 'author',
-          additionalProperties: {}
-        }
-      ],
-      year: '2008',
-      publisher: 'Harvard Business Publishing',
-      publicationSource: 'Boston'
-    }
   end
 
   shared_examples 'it is a CSL report citation' do
