f17b696ebd3afc8faeb6ba51a4822f5f00dee671
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-07 12:27:23 -0800

    PubmedSourceRecord - add a real fixture example for PMID 10000166

--- spec/factories/pubmed_source_records.rb
+++ spec/factories/pubmed_source_records.rb
@@ -8,4 +8,12 @@ FactoryBot.define do
     source_fingerprint 'MyString'
     is_active false
   end
+
+  factory :pubmed_source_record_10000166, parent: :pubmed_source_record do
+    source_data File.read('spec/fixtures/pubmed/pubmed_record_10000166.xml')
+    pmid 10_000_166
+    lock_version 0
+    source_fingerprint 'ae8df3b2a3b1b14d908656bb6a21a708a218a879ffc930ec9ca4f92968525a07'
+    is_active true
+  end
 end
