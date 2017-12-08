e84075f254be660593048d4d0efd65654b4d8468
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-08 12:55:31 -0800

    BibtexIngester - rubocop fixes for class and specs

--- lib/bibtex_ingester.rb
+++ lib/bibtex_ingester.rb
@@ -2,9 +2,9 @@ require 'bibtex'
 require 'citeproc'
 
 class BibtexIngester
-  @@book_type_mapping = %w(book booklet inbook incollection manual techreport)
-  @@article_type_mapping = %w(article misc unpublished)
-  @@inproceedings_type_mapping = %w(conference proceedings inproceedings)
+  BOOK_TYPE_MAPPING = %w(book booklet inbook incollection manual techreport).freeze
+  ARTICLE_TYPE_MAPPING = %w(article misc unpublished).freeze
+  INPROCEEDINGS_TYPE_MAPPING = %w(conference proceedings inproceedings).freeze
 
   def ingest_from_source_directory(directory)
     @batch_dir = directory || Settings.BIBTEX.IMPORT.DIR
@@ -24,13 +24,14 @@ class BibtexIngester
     @total_new_pubs = 0
     @batch_source_records_created_count = 0
     @unidentified_pub_type_count = 0
+    skip_dirs = %w(. ..)
     Dir.open(@batch_dir).each do |batch_dir_name|
-      next if batch_dir_name == '.' || batch_dir_name == '..'
+      next if skip_dirs.include? batch_dir_name
       batch_dir_full_path = "#{@batch_dir}/#{batch_dir_name}"
       next unless File.directory? batch_dir_full_path
       Dir.open(batch_dir_full_path).each do |bibtex_file_name|
         # sunet_id = File.basename(bibtex_file_name, '.*')
-        next if batch_dir_name == '.' || batch_dir_name == '..'
+        next if skip_dirs.include? batch_dir_name
 
         file_full_path = "#{batch_dir_full_path}/#{bibtex_file_name}"
         log_file_full_path = "#{batch_dir_full_path}/#{bibtex_file_name}_import.log"
@@ -98,7 +99,7 @@ class BibtexIngester
         @bibtex_file_logger.error e.backtrace
         @bad_file_error_count += 1
       end
-      if records && !records.empty?
+      if records.present?
         @good_file_count += 1
         records.each do |record|
           @record_count_for_file += 1
@@ -120,12 +121,11 @@ class BibtexIngester
       successful_import: true,
       bibtex_source_data: record.to_s
     }
-    source_attrib_hash[:title] = record.title.to_s unless record['title'].blank?
-    source_attrib_hash[:year] = record.year.to_s unless record['year'].blank?
+    source_attrib_hash[:title] = record.title.to_s if record['title'].present?
+    source_attrib_hash[:year] = record.year.to_s if record['year'].present?
 
     begin
       existing_source_record = BatchUploadedSourceRecord.where(sunet_id: author.sunetid, title: record.title.to_s).first
-
     rescue => e
       @bibtex_import_logger.info "Search for existing batch upload for : #{record} failed probably because of unicode issue."
       @bibtex_import_logger.info "Error: #{e.message}"
@@ -151,12 +151,14 @@ class BibtexIngester
       # create the contribution regardless of whether we created a new pub or are using an existing pub
       Contribution.where(
         author_id: author.id,
-        publication_id: pub.id)
+        publication_id: pub.id
+      )
                   .first_or_create(
                     cap_profile_id: author.cap_profile_id,
                     status: 'approved',
                     visibility: 'private',
-                    featured: false)
+                    featured: false
+                  )
       # have to sync the pub hash to update new information, including new authorship
       pub.sync_publication_hash_and_db
       pub.save
@@ -166,7 +168,6 @@ class BibtexIngester
     end
     @ingested_for_file += 1
     @total_successfully_ingested += 1
-
   rescue => e
     @bibtex_import_logger.info "Record not ingested: #{record}"
     @bibtex_import_logger.info "Error: #{e.message}"
@@ -180,7 +181,7 @@ class BibtexIngester
     year = record['year'].to_s
     title = record['title'].to_s
 
-    if !issn.blank? && !pages.blank? && !year.blank?
+    if issn.present? && pages.present? && year.present?
       begin
         pub = Publication.where("(sciencewire_id is not null OR pmid is not null)
               AND issn =? AND pages=? AND year=? ", issn, pages, year).first
@@ -190,7 +191,7 @@ class BibtexIngester
       end
       @matches_on_issn_count += 1 if pub
     end
-    if pub.nil? && !title.blank? && !pages.blank? && !year.blank?
+    if pub.nil? && title.present? && pages.present? && year.present?
       begin
         pub = Publication.where("(sciencewire_id is not null OR pmid is not null)
               AND title= ? AND year= ? AND pages= ? ", title, year, pages).first
@@ -210,20 +211,20 @@ class BibtexIngester
 
     record_as_hash = {}
     identifiers = []
-    issn = record['issn'].to_s.strip unless record['issn'].blank?
-    isbn = record['isbn'].to_s.strip unless record['isbn'].blank?
-    doi = record['doi'].to_s.strip unless record['doi'].blank?
+    issn = record['issn'].to_s.strip if record['issn'].present?
+    isbn = record['isbn'].to_s.strip if record['isbn'].present?
+    doi = record['doi'].to_s.strip if record['doi'].present?
 
-    unless issn.blank?
+    if issn.present?
       issn_for_id_array = { type: 'issn', id: issn, url: Settings.SULPUB_ID.SEARCHWORKS_URI + issn }
       record_as_hash[:issn] = issn
     end
-    unless isbn.blank?
+    if isbn.present?
       isbn_for_id_array = { type: 'isbn', id: isbn, url: Settings.SULPUB_ID.SEARCHWORKS_URI + isbn }
       record_as_hash[:isbn] = isbn
       identifiers << isbn_for_id_array
     end
-    unless doi.blank?
+    if doi.present?
       doi_for_id_array = { type: 'doi', id: doi, url: "#{Settings.DOI.BASE_URI}#{doi}" }
       identifiers << doi_for_id_array
       record_as_hash[:doi] = doi
@@ -233,61 +234,62 @@ class BibtexIngester
       sul_author_id: author.id,
       status: 'approved',
       visibility: 'public',
-      featured: false }
+      featured: false
+    }
 
-    authorship_hash[:cap_profile_id] = author.cap_profile_id unless author.cap_profile_id.blank?
+    authorship_hash[:cap_profile_id] = author.cap_profile_id if author.cap_profile_id.present?
 
     record_as_hash[:authorship] = [authorship_hash]
 
     record_as_hash[:provenance] = Settings.batch_source
-    record_as_hash[:title] = record.title.to_s.strip unless record['title'].blank?
+    record_as_hash[:title] = record.title.to_s.strip if record['title'].present?
     # unless !record["title"].blank && record["title"].blank? then record_as_hash[:title] = record.chapter.to_s.strip end
-    record_as_hash[:booktitle] = record.booktitle.to_s.strip unless record['booktitle'].blank?
-    unless record['author'].blank?
+    record_as_hash[:booktitle] = record.booktitle.to_s.strip if record['booktitle'].present?
+    if record['author'].present?
       record_as_hash[:author] = record.author.collect { |a| { name: a.to_s } }
       record_as_hash[:allAuthors] = record.author.to_a.join(', ')
     end
 
-    record_as_hash[:editor] = record.editor.to_s.strip unless record['editor'].blank?
-    record_as_hash[:publisher] =  record.publisher.to_s.strip unless record['publisher'].blank?
-    record_as_hash[:year] = record.year.to_s.strip unless record['year'].blank?
-    record_as_hash[:address] = record.address.to_s.strip unless record['address'].blank?
-    record_as_hash[:howpublished] = record.howpublished.to_s.strip unless record['howpublished'].blank?
-    record_as_hash[:edition] = record.edition.to_s.strip unless record['edition'].blank?
-    record_as_hash[:chapter] = record.chapter.to_s.strip unless record['chapter'].blank?
+    record_as_hash[:editor] = record.editor.to_s.strip if record['editor'].present?
+    record_as_hash[:publisher] =  record.publisher.to_s.strip if record['publisher'].present?
+    record_as_hash[:year] = record.year.to_s.strip if record['year'].present?
+    record_as_hash[:address] = record.address.to_s.strip if record['address'].present?
+    record_as_hash[:howpublished] = record.howpublished.to_s.strip if record['howpublished'].present?
+    record_as_hash[:edition] = record.edition.to_s.strip if record['edition'].present?
+    record_as_hash[:chapter] = record.chapter.to_s.strip if record['chapter'].present?
 
     record_as_hash[:type] = sul_document_type
     record_as_hash[:bibtex_type] = record.type.to_s.strip
 
     if sul_document_type == Settings.sul_doc_types.inproceedings
-      conference_hash = { organization: record['organization'].to_s.strip } unless record['organization'].blank?
+      conference_hash = { organization: record['organization'].to_s.strip } if record['organization'].present?
       record_as_hash[:conference] = conference_hash unless conference_hash.nil?
     end
 
-    if sul_document_type == Settings.sul_doc_types.article || !record.journal.blank?
+    if sul_document_type == Settings.sul_doc_types.article || record.journal.present?
       journal_hash = {}
-      journal_hash[:name] = record.journal.to_s.strip unless record['journal'].blank?
-      journal_hash[:volume] = record.volume.to_s.strip unless record['volume'].blank?
-      journal_hash[:issue] = record.issue.to_s.strip unless record['issue'].blank?
-      journal_hash[:articlenumber] = record.number.to_s.strip unless record['number'].blank?
-      journal_hash[:pages] = record.pages.to_s.strip unless record['pages'].blank?
-      journal_hash[:month] = record.month.to_s.strip unless record['month'].blank?
+      journal_hash[:name] = record.journal.to_s.strip if record['journal'].present?
+      journal_hash[:volume] = record.volume.to_s.strip if record['volume'].present?
+      journal_hash[:issue] = record.issue.to_s.strip if record['issue'].present?
+      journal_hash[:articlenumber] = record.number.to_s.strip if record['number'].present?
+      journal_hash[:pages] = record.pages.to_s.strip if record['pages'].present?
+      journal_hash[:month] = record.month.to_s.strip if record['month'].present?
       journal_identifiers = []
-      journal_identifiers << issn_for_id_array unless issn.blank?
+      journal_identifiers << issn_for_id_array if issn.present?
 
       journal_hash[:identifier] = journal_identifiers
       record_as_hash[:journal] = journal_hash unless journal_hash.empty?
-    else
+    elsif record['pages'].present?
       # if this is an article then the pages go in the article object, but if not put it in the main object.
-      record_as_hash[:pages] = record.pages.to_s.strip unless record['pages'].blank?
+      record_as_hash[:pages] = record.pages.to_s.strip
     end
 
     if record['series']
       book_series_hash = {}
       book_series_hash[:identifier] = [issn_for_id_array]
-      book_series_hash[:title] = record.series.to_s.strip  unless record['series'].blank?
-      book_series_hash[:volume] = record.volume.to_s.strip unless record['volume'].blank?
-      book_series_hash[:month] = record.month.to_s.strip unless record['month'].blank?
+      book_series_hash[:title] = record.series.to_s.strip  if record['series'].present?
+      book_series_hash[:volume] = record.volume.to_s.strip if record['volume'].present?
+      book_series_hash[:month] = record.month.to_s.strip if record['month'].present?
       record_as_hash[:series] = book_series_hash unless book_series_hash.empty?
     end
     record_as_hash[:identifier] = identifiers
@@ -295,11 +297,11 @@ class BibtexIngester
   end
 
   def determine_sul_pub_type(bibtex_type)
-    if @@book_type_mapping.include?(bibtex_type)
+    if BOOK_TYPE_MAPPING.include?(bibtex_type)
       Settings.sul_doc_types.book
-    elsif @@article_type_mapping.include?(bibtex_type)
+    elsif ARTICLE_TYPE_MAPPING.include?(bibtex_type)
       Settings.sul_doc_types.article
-    elsif @@inproceedings_type_mapping.include?(bibtex_type)
+    elsif INPROCEEDINGS_TYPE_MAPPING.include?(bibtex_type)
       Settings.sul_doc_types.inproceedings
     end
   end
--- spec/lib/bibtex_ingester_spec.rb
+++ spec/lib/bibtex_ingester_spec.rb
@@ -1,62 +1,68 @@
 
 describe BibtexIngester do
+  subject(:bibtex_ingester) { described_class.new }
+
   let!(:author_with_bibtex) { create :author, sunetid: 'james', cap_profile_id: 333_333 }
-  let!(:bibtex_ingester) { BibtexIngester.new }
+
+  let(:bibtex_batch_path) { Rails.root.join('fixtures', 'bibtex_for_batch').to_s }
+  let(:bibtex_batch_path2) { Rails.root.join('fixtures', 'bibtex_for_batch_2').to_s }
 
   #	puts author_with_bibtex.to_s
 
   describe '#harvest_from_directory_of_bibtex_files' do
     it 'creates pub for each new bibtex record' do
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       end.to change(Publication, :count).by(4)
       expect(author_with_bibtex.publications.size).to eq(4)
     end
 
     it 'associates pubs with author by sunet' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect(author_with_bibtex.publications.size).to eq(4)
     end
 
     it "doesn't duplicate existing publications" do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-      end.to_not change(Publication, :count)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+      end.not_to change(Publication, :count)
     end
+
     it "doesn't duplicate existing publication identifiers" do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-      end.to_not change(PublicationIdentifier, :count)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+      end.not_to change(PublicationIdentifier, :count)
     end
+
     it "doesn't duplicate existing contributions" do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-      end.to_not change(Contribution, :count)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+      end.not_to change(Contribution, :count)
     end
+
     it 'creates new publication identifiers' do
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       end.to change(PublicationIdentifier, :count).by(2)
     end
+
     it 'creates new contributions' do
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       end.to change(Contribution, :count).by(4)
     end
 
     it 'creates BatchUploadedSourceRecord' do
       expect do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       end.to change(BatchUploadedSourceRecord, :count).by(4)
     end
 
     it 'adds citations to pub_hash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.last
       expect(pub.pub_hash[:chicago_citation]).to include(pub.year)
       expect(pub.pub_hash[:apa_citation]).to include(pub.year)
@@ -67,137 +73,142 @@ describe BibtexIngester do
     end
 
     it 'puts issn into journal part of pubhash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly').first
-
       expect(pub.pub_hash[:journal][:identifier].find { |k| k[:type] == 'issn' }[:id]).to include('5555')
     end
 
     it 'adds publication_identifier for isbn' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect(PublicationIdentifier.exists?(identifier_type: 'isbn', identifier_value: 3_233_333)).to be_truthy
     end
 
     it 'adds publication_identifier for DOI' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       expect(PublicationIdentifier.exists?(identifier_type: 'doi', identifier_value: 8_484_848_484)).to be_truthy
     end
 
     it "doesn't add publication_identifier for sulpubid" do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly').first
-
       expect(PublicationIdentifier.exists?(identifier_type: 'SULPubId', identifier_value: pub.id)).to be_falsey
     end
 
     it 'puts SULPubId into identifer part of pubhash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly').first
       expect(pub.pub_hash[:identifier].find { |k| k[:type] == 'SULPubId' }[:id]).to match(pub.id.to_s)
     end
 
     it 'puts DOI into identifer part of pubhash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
       expect(pub.pub_hash[:identifier].find { |k| k[:type] == 'doi' }[:id]).to match('8484848484')
     end
 
     it 'puts isbn into identifer part of pubhash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
       expect(pub.pub_hash[:identifier].find { |k| k[:type] == 'isbn' }[:id]).to match('3233333')
     end
 
     it 'places all of the authors into the allAuthors field of the pub_hash' do
-      bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       pub = Publication.where(title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living').first
       expect(pub.pub_hash[:allAuthors]).to eq 'Goldstein, Mary Kane, Miller, David E., Davies, Sheryl M., Garber, Alan M.'
     end
 
-    context 'when depuping by issn, year, pages' do
-      let!(:existing_matching_pub_issn) { create :publication, pmid: 3_323_434, pub_hash: { title: 'Quelque Titre', type: 'article', pmid: 3_323_434, year: 2002, pages: '295-299', issn: '234234', author: [{ name: 'Jackson, Joe' }], authorship: [{ sul_author_id: 2222, status: 'denied', visibility: 'public', featured: true }] } }
-      let!(:contribution) { create :contribution, author: author_with_bibtex, publication: existing_matching_pub_issn }
+    context 'when deduping by issn, year, pages' do
+      let(:existing_matching_pub_issn) { create :publication, pmid: 3_323_434, pub_hash: { title: 'Quelque Titre', type: 'article', pmid: 3_323_434, year: 2002, pages: '295-299', issn: '234234', author: [{ name: 'Jackson, Joe' }], authorship: [{ sul_author_id: 2222, status: 'denied', visibility: 'public', featured: true }] } }
+      let(:contribution) { create :contribution, author: author_with_bibtex, publication: existing_matching_pub_issn }
+
+      before do
+        contribution # exists
+      end
 
       it "doesn't add duplicate contributions" do
         # puts existing_matching_pub_issn.to_yaml
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(Contribution, :count).by(3)
       end
+
       it "doesn't add duplicate publication identifiers" do
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(PublicationIdentifier, :count).by(0)
       end
 
       it "doesn't duplicate existing publications" do
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(Publication, :count).by(3)
       end
     end
 
-    context 'when depuping by title, year, pages' do
-      let!(:pub_with_same_title) { create :publication, pmid: 332_423_434, pub_hash: { title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living', pmid: 3_323_434, type: 'article', year: 2002, pages: '295-299', author: [{ name: 'Jackson, Joe' }], authorship: [{ sul_author_id: 2222, status: 'denied', visibility: 'public', featured: true }] } }
-      let!(:contribution) { create :contribution, author: author_with_bibtex, publication: pub_with_same_title }
+    context 'when deduping by title, year, pages' do
+      let(:pub_with_same_title) { create :publication, pmid: 332_423_434, pub_hash: { title: 'Quality of Life Assessment Designed for Computer Inexperienced Older Adults: Multimedia Utility Elicitation for Activities of Daily Living', pmid: 3_323_434, type: 'article', year: 2002, pages: '295-299', author: [{ name: 'Jackson, Joe' }], authorship: [{ sul_author_id: 2222, status: 'denied', visibility: 'public', featured: true }] } }
+      let(:contribution) { create :contribution, author: author_with_bibtex, publication: pub_with_same_title }
+
+      before do
+        contribution # exists
+      end
 
       it "doesn't add duplicate contributions" do
         # puts pub_with_title.to_yaml
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(Contribution, :count).by(3)
       end
 
       it "doesn't add duplicate publication identifiers" do
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(PublicationIdentifier, :count).by(0)
       end
 
       it "doesn't duplicate existing publications" do
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
         end.to change(Publication, :count).by(3)
       end
     end
 
     context 'when reimporting with change' do
       # it "updates old record with new title" do
-      # 	bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
+      # 	bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
       # 	oldPub = Publication.where(title: "Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly ").first
       # 	expect {
-      #		bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch_2').to_s)
+      #		bibtex_ingester.ingest_from_source_directory(bibtex_batch_path2)
       #	}.to change { oldPub.pub_hash[:title] }.from("Systematic Review: The Safety and Efficacy of Growth Hormone in the Healthy Elderly ").
       #		to("The new title")
       # end
 
+      before { bibtex_ingester.ingest_from_source_directory(bibtex_batch_path) }
+
       it "doesn't duplicate identifiers" do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-        end.to_not change(PublicationIdentifier, :count)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+        end.not_to change(PublicationIdentifier, :count)
       end
 
       it "doesn't duplicate publications" do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-        end.to_not change(Publication, :count)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+        end.not_to change(Publication, :count)
       end
 
       it "doesn't duplicate contributions" do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
         expect do
-          bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
-        end.to_not change(Contribution, :count)
+          bibtex_ingester.ingest_from_source_directory(bibtex_batch_path)
+        end.not_to change(Contribution, :count)
       end
 
       it 'updates changed issn' do
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch').to_s)
         old_pub = Publication.where(issn: '234234').first
         #	puts "issn:  #{oldPub.pub_hash[:issn]}"
         expect(old_pub.pub_hash[:issn]).to match('234234')
-        bibtex_ingester.ingest_from_source_directory(Rails.root.join('fixtures', 'bibtex_for_batch_2').to_s)
+        bibtex_ingester.ingest_from_source_directory(bibtex_batch_path2)
         old_pub.reload
         expect(old_pub.pub_hash[:issn]).to match('234235')
         # oldPub = Publication.first
