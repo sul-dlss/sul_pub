1f3d44433e158ae8da05076caae2d1e9bb529469
Joe Atzberger <ohiocore@gmail.com>
2017-12-11 15:46:05 -0800

    Allow Config gem to pull from ENV values/overrides
    Conceivably this helps encapsulate overrides for Travis.
    
    Example test you can perform locally, say in `spec/config_spec.rb`:
    ```ruby
    describe 'config gem, how do u work?' do
      it 'overrides from ENV' do
        expect(Settings.WOS.enabled).to eq(true)
      end
      it 'reads from ENV' do
        expect(Settings.whatever).to be_nil
      end
    end
    ```
    Then run these to see the difference in behavior:
    ```
    rspec spec/config_spec.rb # passes
    SETTINGS__WOS__enabled=false rspec spec/config_spec.rb # 1st fails
    SETTINGS__whatever=foobar rspec spec/config_spec.rb    # 2nd fails
    ```

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,6 @@
-Config.setup { }
+Config.setup do |config|
+  config.use_env = true
+  config.env_prefix = 'SETTINGS'
+  config.env_separator = '__'
+  config.env_converter = nil # our keys mix ALLCAPS and downcase
+end
