eea4b1ad08b09b7f750701979bc92c2dfd420241
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-12-11 17:46:00 -0800

    CapHttpClient spec marked pending removal of the old client code

--- spec/lib/cap_http_client_spec.rb
+++ spec/lib/cap_http_client_spec.rb
@@ -9,6 +9,7 @@ describe CapHttpClient do
   end
   describe '#generate_token' do
     it 'sends post request and parse the response' do
+      pending 'removal of the old cap_http_client code'
       stub_request(:post, request_path)
         .to_return(body: { access_token: token }.to_json)
       expect(subject.generate_token).to eq token
