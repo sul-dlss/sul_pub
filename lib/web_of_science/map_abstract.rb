393c7e063111b4cd5f69dc29636544bafe4f16f1
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2018-01-09 14:31:04 -0800

    WebOfScience::MapAbstract - refactor to use new mapper base class

--- lib/web_of_science/map_abstract.rb
+++ lib/web_of_science/map_abstract.rb
@@ -3,37 +3,40 @@ module WebOfScience
   # Map WOS record abstract data into the SUL PubHash data
   class MapAbstract < Mapper
 
-    # @return [Array<String>]
-    def abstracts
-      # Note: rec.doc.xpath(nil).map(&:text) => []
-      rec.doc.xpath(path).map(&:text)
+    attr_reader :abstracts
+
+    # publication abstract details
+    # @return [Hash]
+    def pub_hash
+      return {} if abstracts.empty?
+      # Often there is only one abstract; if there is more than one,
+      # assume the first abstract is the most useful abstract.
+      abstract = abstracts.first.strip
+      case database
+      when 'MEDLINE'
+        { abstract: abstract }
+      else
+        { abstract_restricted: abstract }
+      end
     end
 
     private
 
-      # publication abstract details
-      # @return [Hash]
-      def mapper
-        return {} if abstracts.empty?
-        # Often there is only one abstract; if there is more than one,
-        # assume the first abstract is the most useful abstract.
-        abstract = abstracts.first.strip
-        case rec.database
-        when 'MEDLINE'
-          { abstract: abstract }
-        else
-          { abstract_restricted: abstract }
-        end
+      # Extract content from record, try not to hang onto the entire record
+      # @param rec [WebOfScience::Record]
+      def extract(rec)
+        super(rec)
+        @abstracts = rec.doc.xpath(path).map(&:text)
       end
 
       # The XPath for abstracts data from any WOK database record
       # @return [String, nil]
       def path
-        case rec.database
+        case database
         when 'MEDLINE', 'WOS'
           '/REC/static_data/fullrecord_metadata/abstracts/abstract/abstract_text'
         else
-          rec.logger.error("Unknown WOK database: #{rec.database}")
+          logger.error("Unknown WOK database: #{database}")
           nil
         end
       end
