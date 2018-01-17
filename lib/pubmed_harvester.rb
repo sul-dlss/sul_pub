c38f423c761cceddd2c7906f369258809ea8ee4a
Joe Atzberger <ohiocore@gmail.com>
2018-01-16 16:52:09 -0800

    pubmed_harvester refactoring
    - Booleans, how do they work?
    - No need to use `map!` on a variable and then return it.  Just return the
    result of `map`.
    - use `||=` instead of `unless` clauses
    - various parens and syntactic cleanup

--- lib/pubmed_harvester.rb
+++ lib/pubmed_harvester.rb
@@ -1,8 +1,8 @@
 class PubmedHarvester
-  # Searches locally, then ScienceWire, then Pubmed for a publication by PubmedId
-  # @param [String] pmid PubmedId
-  # @return [Array<Hash>] publication in BibJson format.  Usually for Publication sourcelookup requests
-  #   If no publication found, returns an empty Array
+  # Searches locally (twice), then ScienceWire, then Pubmed for a publication by PubmedId
+  # Is paranoid, searches both publications and publication_identifiers identifiers for pmid
+  # @param [String] pmid Pubmed ID
+  # @return [Array<Hash>] publications in BibJson format or, if none found, an empty Array
   def self.search_all_sources_by_pmid(pmid)
     result = Publication.where(pmid: pmid).to_a # TODO: index manual and batch with pmid?
     result = [Publication.find_by_pmid_pub_id(pmid)].compact if result.empty?
@@ -14,10 +14,9 @@ class PubmedHarvester
       end
 
       if result.none? { |p| p.is_a? Hash }
-        pm_xml = PubmedClient.new.fetch_records_for_pmid_list pmid
+        pm_xml = PubmedClient.new.fetch_records_for_pmid_list(pmid)
         Nokogiri::XML(pm_xml).xpath('//PubmedArticle').each do |doc|
-          pm_rec = PubmedSourceRecord.new
-          result << pm_rec.convert_pubmed_publication_doc_to_hash(doc)
+          result << PubmedSourceRecord.new.convert_pubmed_publication_doc_to_hash(doc)
         end
       end
     end
@@ -26,25 +25,18 @@ class PubmedHarvester
     # Only clean out manual/batch if there's a mix
     if result.size > 1 && result.any? { |p| p.is_a? Hash }
       result.reject! do |pub|
-        if pub.is_a?(Publication) && pub.pub_hash[:provanance] =~ /cap|batch/i
-          true
-        else
-          false
-        end
+        pub.is_a?(Publication) && pub.pub_hash[:provanance] =~ /cap|batch/i
       end
     end
 
     # Turn everything into a pub_hash, then generate citations if needed
-    result.map! do |pub|
-      pub_hash = (pub.pub_hash if pub.respond_to? :pub_hash) || pub
+    result.map do |pub|
+      pub_hash = pub.respond_to?(:pub_hash) ? pub.pub_hash : pub
       cite = Csl::Citation.new(pub_hash)
-
-      pub_hash[:apa_citation] = cite.to_apa_citation unless pub_hash[:apa_citation]
-      pub_hash[:mla_citation] = cite.to_mla_citation unless pub_hash[:mla_citation]
-      pub_hash[:chicago_citation] = cite.to_chicago_citation unless pub_hash[:chicago_citation]
+      pub_hash[:apa_citation] ||= cite.to_apa_citation
+      pub_hash[:mla_citation] ||= cite.to_mla_citation
+      pub_hash[:chicago_citation] ||= cite.to_chicago_citation
       pub_hash
     end
-
-    result
   end
 end
