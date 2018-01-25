07ac5c68cf9025443df3570947f2ba1fd900f47e
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2018-01-24 17:36:48 -0800

    WebOfScience::Harvester - profiling script

---
+++ script/wos_harvest_profiling.rb
@@ -0,0 +1,15 @@
+
+# Usage
+# RAILS_ENV=production bundle exec rails runner ./script/wos_harvest_profiling.rb > log/wos_harvest_profiling.txt
+
+require 'ruby-prof'
+RubyProf.start
+
+authors = Author.where(active_in_cap: true, cap_import_enabled: true).limit(15)
+WebOfScience.harvester.harvest(authors)
+
+result = RubyProf.stop
+
+# print a flat profile to text
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(STDOUT)
