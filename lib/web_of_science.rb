3908272293d9f8d2133e3492ce205f58b2e2f78c
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2018-01-30 12:28:30 -0800

    WebOfScience - rubocop autofix Style/PercentLiteralDelimiters

--- lib/web_of_science.rb
+++ lib/web_of_science.rb
@@ -30,7 +30,7 @@ module WebOfScience
   # Fetch a single publication and parse and ensure we have a correct response.
   # We check in steps that the response is XML and it includes the correct content.
   def self.working?
-    uids = %w(WOS:A1976BW18000001 WOS:A1972N549400003)
+    uids = %w[WOS:A1976BW18000001 WOS:A1972N549400003]
     retriever = queries.retrieve_by_id(uids)
     records = retriever.next_batch
     raise 'WebOfScience found no records' unless records.is_a?(WebOfScience::Records) && records.count > 0
