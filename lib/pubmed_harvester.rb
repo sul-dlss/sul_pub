74cc49a94b49e8a85b25b97f6e9a9537e3994ff7
Joe Atzberger <ohiocore@gmail.com>
2018-01-30 15:55:35 -0800

    Pubmed searching to WOS w/ tests
    - Broke out the remote searching to a private_class_method.
    - Changed the expectations w/ regards to local hits based on analysis
    of actual duplicate values in #693. No reason to present the user with
    indifferentiable duplicate records.
    - Honor configs

--- lib/pubmed_harvester.rb
+++ lib/pubmed_harvester.rb
@@ -1,42 +1,49 @@
 class PubmedHarvester
-  # Searches locally (twice), then ScienceWire, then Pubmed for a publication by PubmedId
+  # Searches locally (twice), then ScienceWire/WOS, then Pubmed for a publication by PubmedId
   # Is paranoid, searches both publications and publication_identifiers identifiers for pmid
   # @param [String] pmid Pubmed ID
   # @return [Array<Hash>] publications in BibJson format or, if none found, an empty Array
   def self.search_all_sources_by_pmid(pmid)
-    result = Publication.where(pmid: pmid).to_a # TODO: index manual and batch with pmid?
-    result = [Publication.find_by_pmid_pub_id(pmid)].compact if result.empty?
+    pub = Publication.find_by(pmid: pmid) || Publication.find_by_pmid_pub_id(pmid)
+    return [pub.pub_hash] if pub && pub.authoritative_pmid_source?
+    result = fetch_remote_pubmed(pmid)
+    return result unless result.empty?
+    [pub.pub_hash] # non-authoritative local hit
+  end
 
-    if result.none?(&:authoritative_pmid_source?)
+  # @param [String] pmid Pubmed ID
+  # @return [Array<Hash>] pub_hashes or an empty Array
+  def self.fetch_remote_pubmed(pmid)
+    if Settings.SCIENCEWIRE.enabled
       sw_records_doc = ScienceWireClient.new.pull_records_from_sciencewire_for_pmids(pmid)
-      sw_records_doc.xpath('//PublicationItem').each do |sw_doc|
-        result << SciencewireSourceRecord.convert_sw_publication_doc_to_hash(sw_doc)
+      result = sw_records_doc.xpath('//PublicationItem').map do |sw_doc|
+        add_citation(SciencewireSourceRecord.convert_sw_publication_doc_to_hash(sw_doc))
       end
-
-      if result.none? { |p| p.is_a? Hash }
-        pm_xml = PubmedClient.new.fetch_records_for_pmid_list(pmid)
-        Nokogiri::XML(pm_xml).xpath('//PubmedArticle').each do |doc|
-          result << PubmedSourceRecord.new.convert_pubmed_publication_doc_to_hash(doc)
-        end
-      end
+      return result unless result.empty?
     end
 
-    # If we have more than one result, we could have a mix of local and pubmed/SW records
-    # Only clean out manual/batch if there's a mix
-    if result.size > 1 && result.any? { |p| p.is_a? Hash }
-      result.reject! do |pub|
-        pub.is_a?(Publication) && !pub.authoritative_pmid_source?
-      end
+    # note: only works because all results expected to fit inside one "batch"
+    if Settings.WOS.enabled
+      result = WebOfScience.queries.retrieve_by_pmid([pmid]).next_batch.map { |rec| add_citation(rec.pub_hash) }
+      return result unless result.empty?
     end
 
-    # Turn everything into a pub_hash, then generate citations if needed
-    result.map do |pub|
-      pub_hash = pub.respond_to?(:pub_hash) ? pub.pub_hash : pub
-      cite = Csl::Citation.new(pub_hash)
-      pub_hash[:apa_citation] ||= cite.to_apa_citation
-      pub_hash[:mla_citation] ||= cite.to_mla_citation
-      pub_hash[:chicago_citation] ||= cite.to_chicago_citation
-      pub_hash
+    # PubMed, oddly enough, the last resort
+    pm_xml = PubmedClient.new.fetch_records_for_pmid_list(pmid)
+    Nokogiri::XML(pm_xml).xpath('//PubmedArticle').map do |doc|
+      add_citation(PubmedSourceRecord.new.convert_pubmed_publication_doc_to_hash(doc))
     end
   end
+  private_class_method :fetch_remote_pubmed
+
+  # @param [Hash] pub_hash modifies passed hash to include citation k/v pairs
+  # @return [Hash] the same modified hash
+  def self.add_citation(pub_hash)
+    cite = Csl::Citation.new(pub_hash)
+    pub_hash[:apa_citation] ||= cite.to_apa_citation
+    pub_hash[:mla_citation] ||= cite.to_mla_citation
+    pub_hash[:chicago_citation] ||= cite.to_chicago_citation
+    pub_hash
+  end
+  private_class_method :add_citation
 end
--- spec/lib/pubmed_harvester_spec.rb
+++ spec/lib/pubmed_harvester_spec.rb
@@ -12,14 +12,15 @@ describe PubmedHarvester, :vcr do
       provenance: 'pubmed'
     }
   end
-  let!(:publication) { create(:publication, pmid: 10_048_354, pub_hash: pub_hash) }
 
   before(:each) do
-    allow(Settings.SCIENCEWIRE).to receive(:enabled).and_return(true) # default
-    allow(Settings.WOS).to receive(:enabled).and_return(false) # default
+    allow(Settings.SCIENCEWIRE).to receive(:enabled).and_return(true) # legacy tests VCR'd w/ SW + PubMed
+    allow(Settings.WOS).to receive(:enabled).and_return(false) # but not WOS
   end
 
   describe '.search_all_sources_by_pmid' do
+    let!(:publication) { create(:publication, pmid: 10_048_354, pub_hash: pub_hash) }
+
     it 'searches for a local Publication by pmid and returns a pubhash' do
       expect(ScienceWireClient).not_to receive(:new)
       expect(PubmedClient).not_to receive(:new)
@@ -39,35 +40,101 @@ describe PubmedHarvester, :vcr do
       skip 'find an example pubmid not yet in sw'
       # This pmid might eventually show up in SW.  If that's the case, search the recent production logs for publication sourcelookups with this format:
       # /publications/sourcelookup?pmid=
-      h = PubmedHarvester.search_all_sources_by_pmid(24_930_130)
-      expect(h.first[:provenance]).to eq('pubmed')
-      expect(h.first[:identifier]).to include(type: 'doi', id: '10.1038/nmeth.2999', url: 'https://dx.doi.org/10.1038/nmeth.2999')
-      expect(h.first[:chicago_citation]).to match(/Chemically Defined Generation/)
+      h = PubmedHarvester.search_all_sources_by_pmid(24_930_130).first
+      expect(h[:provenance]).to eq('pubmed')
+      expect(h[:identifier]).to include(type: 'doi', id: '10.1038/nmeth.2999', url: 'https://dx.doi.org/10.1038/nmeth.2999')
+      expect(h[:chicago_citation]).to match(/Chemically Defined Generation/)
     end
 
-    context 'mix of local plus SW/pubmed results' do
-      it 'filters out batch/manual pubs from the resultset if SW/pubmed records were found' do
+    context 'local hits of varied provenance' do
+      before { allow(PubmedHarvester).to receive(:fetch_remote_pubmed).and_return([]) }
+
+      it 'filters out batch/manual pubs from the resultset if previous SW/pubmed records were found' do
         Publication.create!(
           pub_hash: pub_hash.merge(provenance: 'batch'),
           pmid: 10_487_815
         )
-        expect(PubmedClient).not_to receive(:new)
         h = PubmedHarvester.search_all_sources_by_pmid(10_487_815)
         expect(h.size).to eq 1
-        expect(h.first[:sw_id]).to eq '10340243'
       end
 
-      it 'does not do any filtering with a resultset of 2 manual/batch pubs' do
-        publication.pmid = 99_999_999 # Pubmed ID that does not exist
-        publication.pub_hash = pub_hash.merge(provenance: 'cap')
-        publication.save!
-        Publication.create!(
-          pub_hash: pub_hash.merge(title: 'batch pub', provenance: 'batch'),
-          pmid: 99_999_999
-        )
-        h = PubmedHarvester.search_all_sources_by_pmid(99_999_999)
-        expect(h.size).to eq 2
-        expect(h.map { |hash| hash[:title] }).to include('batch pub', 'some title')
+      context 'duplicate PMIDs' do
+        before do
+          publication.pmid = 99_999_999 # Pubmed ID that does not exist
+          publication.pub_hash = pub_hash.merge(provenance: 'cap')
+          publication.save!
+          Publication.create!(
+            pub_hash: pub_hash.merge(title: 'batch pub', provenance: 'batch'),
+            pmid: 99_999_999
+          )
+        end
+
+        it 'returns first of local matches' do
+          h = PubmedHarvester.search_all_sources_by_pmid(99_999_999)
+          expect(h.size).to eq 1
+        end
+      end
+    end
+  end
+
+  # private
+  describe '.fetch_remote_pubmed' do
+    subject(:hits) { described_class.send(:fetch_remote_pubmed, 24_930_130) } # backed by VCR cassettes for each provider
+
+    before(:each) do
+      allow(Settings.SCIENCEWIRE).to receive(:enabled).and_return(false) # default
+      allow(Settings.WOS).to receive(:enabled).and_return(false) # default
+    end
+
+    context 'WOS and ScienceWire, both disabled' do
+      it 'searches only pubmed' do
+        expect(ScienceWireClient).not_to receive(:new)
+        expect(WebOfScience).not_to receive(:queries)
+        expect(hits.size).to eq(1)
+        expect(hits.first[:title]).to eq('Chemically defined generation of human cardiomyocytes.')
+        expect(hits.first[:chicago_citation]).to match(/Chemically Defined Generation/)
+      end
+    end
+
+    context 'WOS disabled, ScienceWire enabled' do
+      before { allow(Settings.SCIENCEWIRE).to receive(:enabled).and_return(true) }
+
+      it 'searches only ScienceWire' do
+        expect(WebOfScience).not_to receive(:queries)
+        expect(PubmedClient).not_to receive(:new)
+        expect(hits.size).to eq(1)
+        expect(hits.first[:title]).to eq('Chemically defined generation of human cardiomyocytes') # no period, meh
+        expect(hits.first[:chicago_citation]).to match(/Chemically Defined Generation/)
+      end
+    end
+
+    context 'WOS enabled, ScienceWire disabled' do
+      before { allow(Settings.WOS).to receive(:enabled).and_return(true) }
+
+      it 'searches only WOS' do
+        expect(ScienceWireClient).not_to receive(:new)
+        expect(PubmedClient).not_to receive(:new)
+        expect(hits.size).to eq(1)
+        expect(hits.first[:title]).to eq('Chemically defined generation of human cardiomyocytes.')
+        expect(hits.first[:chicago_citation]).to match(/Chemically Defined Generation/)
+      end
+    end
+
+    context 'Everything enabled' do
+      before(:each) do
+        allow(Settings.SCIENCEWIRE).to receive(:enabled).and_return(true)
+        allow(Settings.WOS).to receive(:enabled).and_return(true)
+      end
+
+      it 'without hits, fails over across services' do
+        expect(WebOfScience.queries).to receive(:retrieve_by_pmid)
+          .with([24_930_130])
+          .and_return(instance_double(WebOfScience::Retriever, next_batch: WebOfScience::Records.new(records: '<xml/>')))
+        expect(PubmedClient).to receive(:new)
+          .and_return(instance_double(PubmedClient, fetch_records_for_pmid_list: []))
+        expect(ScienceWireClient).to receive(:new)
+          .and_return(instance_double(ScienceWireClient, pull_records_from_sciencewire_for_pmids: Nokogiri::XML('<xml/>')))
+        expect(hits).to eq([])
       end
     end
   end
