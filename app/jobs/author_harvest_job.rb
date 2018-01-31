7257f1d98b638e825dc5b4a932059886d4051803
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2018-01-31 12:50:59 -0800

    AuthorHarvestJob - extract method for sciencewire, using settings to enable it

--- app/jobs/author_harvest_job.rb
+++ app/jobs/author_harvest_job.rb
@@ -1,24 +1,31 @@
 class AuthorHarvestJob < ActiveJob::Base
   queue_as :default
 
-  ##
   # Performs an asynchronous harvest and save for a given Author
   # @param [String] cap_profile_id
   # @param [Boolean] harvest_alternate_names
   def perform(cap_profile_id, harvest_alternate_names: false)
-    harvester = ScienceWireHarvester.new
-    harvester.use_author_identities = harvest_alternate_names
-    author = Author.where(cap_profile_id: cap_profile_id).first
+    author = Author.find_by(cap_profile_id: cap_profile_id)
     author ||= Author.fetch_from_cap_and_create(cap_profile_id)
-    harvester.harvest_pubs_for_author_ids author.id
-    pubs = Contribution.where(author_id: author.id).map(&:publication).each do |p|
-      logger.info "publication #{p.id}: #{p.pub_hash[:apa_citation]}"
-    end
-    logger.info "Number of publications #{pubs.count}"
+    raise "Could not find or fetch author: #{cap_profile_id}" unless author.is_a?(Author)
+    sciencewire(author, harvest_alternate_names)
   rescue => e
     msg = "AuthorHarvestJob.perform(cap_profile_id=#{cap_profile_id}, harvest_alternate_names=#{harvest_alternate_names})"
     NotificationManager.log_exception(logger, msg, e)
     Honeybadger.notify(e, context: { message: msg })
     raise
   end
+
+  private
+
+    def sciencewire(author, harvest_alternate_names)
+      return unless Settings.SCIENCEWIRE.enabled
+      harvester = ScienceWireHarvester.new
+      harvester.use_author_identities = harvest_alternate_names
+      harvester.harvest_pubs_for_author_ids author.id
+      pubs = Contribution.where(author_id: author.id).map(&:publication).each do |p|
+        logger.info "publication #{p.id}: #{p.pub_hash[:apa_citation]}"
+      end
+      logger.info "Number of publications #{pubs.count}"
+    end
 end
