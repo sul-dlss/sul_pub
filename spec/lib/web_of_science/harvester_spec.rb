497843a7edcd4d9678045c90af016004931ca835
Joe Atzberger <ohiocore@gmail.com>
2018-03-08 19:48:56 -0800

    Add spec for guaranteeing backfill of Contribs for existing pubs

--- spec/lib/web_of_science/harvester_spec.rb
+++ spec/lib/web_of_science/harvester_spec.rb
@@ -12,77 +12,107 @@ describe WebOfScience::Harvester do
 
   let(:author) { create :russ_altman }
   let(:wos_uids) { %w(WOS:A1976BW18000001 WOS:A1972N549400003) } # from wos_retrieve_by_id_response.xml
-  # let(:wos_rec) { WebOfScience::Record.new(record: File.read('spec/fixtures/wos_client/wos_record_A1976BW18000001.xml')) }
   let(:author_name_response) { File.read('spec/fixtures/wos_client/wos_harvest_author_name_response.xml') }
   let(:retrieve_by_id_response) { File.read('spec/fixtures/wos_client/wos_retrieve_by_id_response.xml') }
+  let(:wos_rec) { WebOfScience::Record.new(record: File.read('spec/fixtures/wos_client/wos_record_A1972N549400003.xml')) }
   let(:any_records_will_do) do
     WebOfScience::Records.new(encoded_records: File.read('spec/fixtures/wos_client/medline_encoded_records.html'))
   end
 
-  before do
-    allow(WebOfScience).to receive(:client).and_return(WebOfScience::Client.new('secret'))
-    savon.expects(:authenticate).returns(File.read('spec/fixtures/wos_client/authenticate.xml'))
-  end
-
-  shared_examples 'it_can_process_records' do
-    it 'creates new WebOfScienceSourceRecord and author.contributions' do
-      expect { harvest_process }.to change { [WebOfScienceSourceRecord.count, author.contributions.count] }
-    end
-  end
-
-  describe '#harvest' do
+  context 'savon' do
     before do
-      savon.expects(:search).with(message: :any).returns(author_name_response)
-      savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response)
+      allow(WebOfScience).to receive(:client).and_return(WebOfScience::Client.new('secret'))
+      savon.expects(:authenticate).returns(File.read('spec/fixtures/wos_client/authenticate.xml'))
     end
 
-    let(:harvest_process) { harvester.harvest([author]) }
-
-    it_behaves_like 'it_can_process_records'
-
-    it 'logs exceptions for processing an author' do
-      processor = WebOfScience::ProcessRecords.new(author, any_records_will_do)
-      allow(processor).to receive(:execute).and_raise(RuntimeError)
-      allow(WebOfScience::ProcessRecords).to receive(:new).and_return(processor)
-      expect(NotificationManager).to receive(:error)
-      harvest_process
+    shared_examples 'it_can_process_records' do
+      it 'creates new WebOfScienceSourceRecord and author.contributions' do
+        expect { harvest_process }.to change { [WebOfScienceSourceRecord.count, author.contributions.count] }
+      end
     end
 
-    context 'with existing publication and/or contribution data' do
-      let(:wos_rec) { WebOfScience::Record.new(record: File.read('spec/fixtures/wos_client/wos_record_A1972N549400003.xml')) }
-      let(:retrieve_by_id_response) { File.read('spec/fixtures/wos_client/wos_record_A1976BW18000001_response.xml') }
+    describe '#harvest' do
       before do
-        Publication.new(active: true, pub_hash: wos_rec.pub_hash, wos_uid: wos_rec.uid) do |pub|
-          pub.sync_publication_hash_and_db # callbacks create PublicationIdentifiers etc.
-          pub.save!
-          pub.contributions.find_or_create_by!(
-            author_id: author.id, cap_profile_id: author.cap_profile_id,
-            featured: false, status: 'new', visibility: 'private'
-          )
+        savon.expects(:search).with(message: :any).returns(author_name_response)
+        savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response)
+      end
+
+      let(:harvest_process) { harvester.harvest([author]) }
+
+      it_behaves_like 'it_can_process_records'
+
+      it 'logs exceptions for processing an author' do
+        processor = WebOfScience::ProcessRecords.new(author, any_records_will_do)
+        allow(processor).to receive(:execute).and_raise(RuntimeError)
+        allow(WebOfScience::ProcessRecords).to receive(:new).and_return(processor)
+        expect(NotificationManager).to receive(:error)
+        harvest_process
+      end
+
+      context 'with existing publication and/or contribution data' do
+        let(:retrieve_by_id_response) { File.read('spec/fixtures/wos_client/wos_record_A1976BW18000001_response.xml') }
+        before do
+          Publication.new(active: true, pub_hash: wos_rec.pub_hash, wos_uid: wos_rec.uid) do |pub|
+            pub.sync_publication_hash_and_db # callbacks create PublicationIdentifiers etc.
+            pub.save!
+            pub.contributions.find_or_create_by!(
+              author_id: author.id, cap_profile_id: author.cap_profile_id,
+              featured: false, status: 'new', visibility: 'private'
+            )
+          end
+        end
+
+        # Use a new record WITHOUT a publication for WOS:A1976BW18000001, from wos_retrieve_by_id_response.xml
+        it 'processes records that have no publication' do
+          expect { harvest_process }.to change { Publication.find_by(wos_uid: 'WOS:A1976BW18000001') }.from(nil).to(Publication)
         end
       end
+    end
 
-      # Use a new record WITHOUT a publication for WOS:A1976BW18000001, from wos_retrieve_by_id_response.xml
-      it 'processes records that have no publication' do
-        expect { harvest_process }.to change { Publication.find_by(wos_uid: 'WOS:A1976BW18000001') }.from(nil).to(Publication)
+    describe '#process_author' do
+      let(:harvest_process) { harvester.process_author(author) }
+      before do
+        savon.expects(:search).with(message: :any).returns(author_name_response)
+        savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response)
+      end
+
+      it_behaves_like 'it_can_process_records'
+    end
+
+    describe '#process_uids' do
+      let(:harvest_process) { harvester.process_uids(author, wos_uids) }
+      context 'savon' do
+        before { savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response) }
+        it_behaves_like 'it_can_process_records'
       end
     end
   end
 
-  describe '#process_author' do
-    let(:harvest_process) { harvester.process_author(author) }
-    before do
-      savon.expects(:search).with(message: :any).returns(author_name_response)
-      savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response)
+  context 'non savon' do
+    describe '#process_uids' do
+      context 'when matching Publication is unpaired with author' do
+        let(:wos_rec_01) { WebOfScience::Record.new(record: File.read('spec/fixtures/wos_client/wos_record_A1976BW18000001.xml')) }
+        before do
+          Publication.new(active: true, pub_hash: wos_rec.pub_hash, wos_uid: wos_rec.uid, pubhash_needs_update: true) do |pub|
+            pub.save!
+            pub.contributions.find_or_create_by!(
+              author_id: author.id, cap_profile_id: author.cap_profile_id,
+              featured: false, status: 'new', visibility: 'private'
+            )
+          end
+          Publication.new(active: true, pub_hash: wos_rec_01.pub_hash, wos_uid: wos_rec_01.uid) do |pub|
+            pub.sync_publication_hash_and_db
+            pub.save! # no contributions
+          end
+        end
+
+        it 'creates new Contribution' do
+          # note: new source record not added when matching pub already found
+          expect(author.publications.map(&:wos_uid)).not_to include(wos_uids.first)
+          expect { harvester.process_uids(author, wos_uids) }.to change { author.contributions.count }.from(1).to(2)
+          expect(author.publications.reload.map(&:wos_uid)).to include(*wos_uids)
+        end
+      end
     end
-
-    it_behaves_like 'it_can_process_records'
-  end
-
-  describe '#process_uids' do
-    let(:harvest_process) { harvester.process_uids(author, wos_uids) }
-    before { savon.expects(:retrieve_by_id).with(message: :any).returns(retrieve_by_id_response) }
-
-    it_behaves_like 'it_can_process_records'
   end
 end
