b6acda2eb26a046ae8964940a6076c73e8932d20
Joe Atzberger <ohiocore@gmail.com>
2018-03-13 19:13:23 -0700

    Remove 2 unused methods, subsume another

--- lib/web_of_science/contributions.rb
+++ lib/web_of_science/contributions.rb
@@ -8,43 +8,19 @@ module WebOfScience
     # existing publication without one for the author in question.
     # @param author [Author]
     # @param uids [Array<String>]
-    # @return [Array<String>] uids that already have a contribution
+    # @return [Array<String>] uids guaranteed to have matching Publication and Contribution
     def author_contributions(author, uids)
-      contrib_uids = []
-      Publication.where(wos_uid: uids).find_each do |pub|
-        contrib = find_or_create_contribution(author, pub)
-        contrib_uids << pub.wos_uid if contrib.persisted?
+      Publication.where(wos_uid: uids).find_each.map do |pub|
+        pub.contributions.find_or_create_by!(author_id: author.id) do |contrib|
+          contrib.assign_attributes(
+            cap_profile_id: author.cap_profile_id,
+            featured: false, status: 'new', visibility: 'private'
+          )
+          pub.pubhash_needs_update! # Add to pub_hash[:authorship]
+          pub.save! # contrib.save! not needed
+        end
+        pub.wos_uid
       end
-      contrib_uids
-    end
-
-    # Find any matching contributions by author and WOS-UID; create a contribution for any
-    # existing publication without one for the author in question.
-    # @param author [Author]
-    # @param uid [String]
-    # @return [Contribution, nil]
-    def author_contribution(author, uid)
-      pub = Publication.find(wos_uid: uid)
-      return if pub.nil?
-      find_or_create_contribution(author, pub)
-    end
-
-    # Find or create a new contribution to a publication for author.
-    # @param [Author]
-    # @param [Publication]
-    # @return [Contribution]
-    def find_or_create_contribution(author, publication)
-      publication.contributions.find_or_create_by!(author_id: author.id) do |contrib|
-        contrib.assign_attributes(
-          cap_profile_id: author.cap_profile_id,
-          featured: false, status: 'new', visibility: 'private'
-        )
-        publication.pubhash_needs_update! # Add to pub_hash[:authorship]
-        publication.save! # contrib.save! not needed
-      end
-    rescue ActiveRecord::ActiveRecordError => err
-      message = "Failed to find/create contribution for author: #{author.id}, pub: #{publication.id}"
-      NotificationManager.error(err, message, self)
     end
 
     # Does record have a contribution for this author? (based on matching PublicationIdentifiers)
@@ -71,19 +47,6 @@ module WebOfScience
       ).first
       return unless pub
       find_or_create_contribution(author, pub)
-    end
-
-    # Find any matching contribution by author and PublicationIdentifier
-    # @param author [Author]
-    # @param type [String]
-    # @param value [String]
-    # @return [Boolean] contribution exists
-    def contribution_by_identifier?(author, type, value)
-      return false if type.blank? || value.blank?
-      pub = Publication.includes(:publication_identifiers)
-                       .find_by("publication_identifiers.identifier_type": type, "publication_identifiers.identifier_value": value)
-      return false unless pub
-      find_or_create_contribution(author, pub) ? true : false
     end
   end
 end
