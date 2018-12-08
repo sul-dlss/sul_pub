c25f769564ba8c22c1159a37c3e4c87854dfa469
Tony Zanella <tony.zanella@gmail.com>
2018-12-07 17:26:39 -0800

    sul-pub-dev is gone

--- config/deploy/dev.rb
+++
@@ -1,7 +0,0 @@
-server 'sul-pub-dev.stanford.edu', user: 'pub', roles: %w(web db app)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :rails_env, 'development'
-
-set :bundle_without, %w(test).join(' ')
