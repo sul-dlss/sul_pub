b87b5750162e874c8ee942ee5414c61290d44084
Peter Mangiafico <peter@mangiafico.org>
2019-10-30 11:15:43 -0700

    add new deploy targets for Profiles team to test against

---
+++ config/deploy/cap-uat.rb
@@ -0,0 +1,12 @@
+# This will become the new UAT server (currently called `cap-qa`).  It will initially be used by the Profiles
+#   team for testing their Oracle upgrade against.  Later we will remove the `cap-qa` host and deploy target and keep this in its place.
+# Oct 30 2019, P Mangiafico
+server 'sul-pub-cap-uat.stanford.edu', user: 'pub', roles: %w(web db app harvester_qa external_monitor)
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :rails_env, 'production'
+
+set :bundle_without, %w(test development).join(' ')
+
+set :log_level, :info
