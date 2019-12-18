d11377080c54cddbb95a46ca581ce62ec016bfb0
Peter Mangiafico <peter@mangiafico.org>
2019-12-17 16:16:56 -0800

    enable cron jobs on new dev and uat servers; rename qa role to uat

--- config/deploy/cap-uat.rb
+++ config/deploy/cap-uat.rb
@@ -1,7 +1,7 @@
 # This will become the new UAT server (currently called `cap-qa`).  It will initially be used by the Profiles
 #   team for testing their Oracle upgrade against.  Later we will remove the `cap-qa` host and deploy target and keep this in its place.
 # Oct 30 2019, P Mangiafico
-server 'sul-pub-cap-uat.stanford.edu', user: 'pub', roles: %w(web db app external_monitor)
+server 'sul-pub-cap-uat.stanford.edu', user: 'pub', roles: %w(web db app harvester_uat external_monitor)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
