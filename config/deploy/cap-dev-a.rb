e2d43ab8aeed20d5c565ff6a82fd01d4ea5b48c8
Peter Mangiafico <peter@mangiafico.org>
2021-04-01 09:24:35 -0700

    remove unneeded environments; setup cron jobs appropriately

--- config/deploy/cap-dev-a.rb
+++
@@ -1,8 +0,0 @@
-# see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
-server 'sul-pub-cap-dev-a.stanford.edu', user: 'pub', roles: %w(web db harvester_dev_a app)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-
-set :rails_env, 'production'
-
-set :bundle_without, %w(development test).join(' ')
--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,5 +1,5 @@
 # see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
-server 'sul-pub-cap-qa.stanford.edu', user: 'pub', roles: %w(web db app harvester_qa external_monitor)
+server 'sul-pub-cap-uat.stanford.edu', user: 'pub', roles: %w(web db app harvester_qa external_monitor)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
