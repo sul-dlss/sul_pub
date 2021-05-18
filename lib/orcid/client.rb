48f8c51ff6c62587ba49323e4e4e3e102b993e62
Justin Littman <justinlittman@stanford.edu>
2021-05-18 16:54:17 -0400

    Adds support for adding new work to Orcid.
    closes #1258

--- lib/orcid/client.rb
+++ lib/orcid/client.rb
@@ -7,38 +7,70 @@ module Orcid
   class Client
     # Fetch the works for a researcher.
     # Model for the response: https://pub.orcid.org/v3.0/#!/Development_Public_API_v3.0/viewWorksv3
+    # @param [string] ORCID ID for the researcher
+    # @return [Hash]
     def fetch_works(orcidid)
-      response = conn.get("/v3.0/#{orcidid[-19, 19]}/works")
+      response = conn.get("/v3.0/#{base_orcidid(orcidid)}/works")
       raise "ORCID.org API returned #{response.status}" if response.status != 200
 
       JSON.parse(response.body).with_indifferent_access
     end
 
+    # Add a new work for a researcher.
+    # @param [string] ORCID ID for the researcher
+    # @param [Hash] work in correct data structure for ORCID work
+    # @param [string] access token
+    # @return [string] put-code
+    def add_work(orcidid, work, token)
+      response = conn_with_token(token).post("/v3.0/#{base_orcidid(orcidid)}/work",
+                                             work.to_json,
+                                             'Content-Type' => 'application/json')
+
+      case response.status
+      when 201
+        response['Location'].match(%r{work/(\d+)})[1]
+      when 409
+        match = response.body.match(/put-code (\d+)\./)
+        raise 'ORCID.org API returned a 409, but could not find put-code' unless match
+
+        match[1]
+      else
+        raise "ORCID.org API returned #{response.status}"
+      end
+    end
+
     private
 
-    def token
+    def base_orcidid(orcidid)
+      orcidid[-19, 19]
+    end
+
+    def client_token
       client = OAuth2::Client.new(Settings.ORCID.CLIENT_ID, Settings.ORCID.CLIENT_SECRET, site: Settings.ORCID.BASE_AUTH_URL)
       token = client.client_credentials.get_token({ scope: '/read-public' })
-      "Bearer #{token.token}"
+      token.token
     end
 
     # @return [Faraday::Connection]
     def conn
-      @conn ||= begin
-        conn = Faraday.new(url: Settings.ORCID.BASE_URL) do |faraday|
-          faraday.request :retry, max: 3,
-                                  interval: 0.5,
-                                  interval_randomness: 0.5,
-                                  backoff_factor: 2
-          faraday.adapter :httpclient
-        end
-        conn.options.timeout = 500
-        conn.options.open_timeout = 10
-        conn.headers[:user_agent] = 'stanford-library-sul-pub'
-        conn.headers[:accept] = 'application/json'
-        conn.headers[:authorization] = token
-        conn
+      @conn ||= conn_with_token(client_token)
+    end
+
+    # @return [Faraday::Connection]
+    def conn_with_token(token)
+      conn = Faraday.new(url: Settings.ORCID.BASE_URL) do |faraday|
+        faraday.request :retry, max: 3,
+                                interval: 0.5,
+                                interval_randomness: 0.5,
+                                backoff_factor: 2
+        faraday.adapter :httpclient
       end
+      conn.options.timeout = 500
+      conn.options.open_timeout = 10
+      conn.headers[:user_agent] = 'stanford-library-sul-pub'
+      conn.headers[:accept] = 'application/json'
+      conn.headers[:authorization] = "Bearer #{token}"
+      conn
     end
   end
 end
--- spec/lib/orcid/client_spec.rb
+++ spec/lib/orcid/client_spec.rb
@@ -20,4 +20,115 @@ describe Orcid::Client do
       end
     end
   end
+
+  describe '#add_work' do
+    let(:work) do
+      {
+        title: {
+          title: {
+            value: 'Twitter Makes It Worse: Political Journalists, Gendered Echo Chambers, and the Amplification of Gender Bias'
+          },
+          subtitle: nil,
+          "translated-title": nil
+        },
+        "journal-title": {
+          value: 'The International Journal of Press/Politics'
+        },
+        "short-description": nil,
+        citation: {
+          "citation-type": 'bibtex',
+          "citation-value": "@article{Usher_2018,\n\tdoi = {10.1177/1940161218781254},\n\turl = {https://doi.org/10.1177%2F1940161218781254},\n\tyear = 2018,\n\tmonth = {jun},\n\tpublisher = {{SAGE} Publications},\n\tvolume = {23},\n\tnumber = {3},\n\tpages = {324--344},\n\tauthor = {Nikki Usher and Jesse Holcomb and Justin Littman},\n\ttitle = {Twitter Makes It Worse: Political Journalists, Gendered Echo Chambers, and the Amplification of Gender Bias},\n\tjournal = {The International Journal of Press/Politics}\n}"
+        },
+        type: 'journal-article',
+        "publication-date": {
+          year: {
+            value: '2018'
+          },
+          month: {
+            value: '07'
+          },
+          day: {
+            value: '24'
+          }
+        },
+        "external-ids": {
+          "external-id": [
+            {
+              "external-id-type": 'doi',
+              "external-id-value": '10.1177/1940161218781254',
+              "external-id-normalized": {
+                value: '10.1177/1940161218781254',
+                transient: true
+              },
+              "external-id-normalized-error": nil,
+              "external-id-url": {
+                value: 'https://doi.org/10.1177/1940161218781254'
+              },
+              "external-id-relationship": 'self'
+            }
+          ]
+        },
+        url: {
+          value: 'https://doi.org/10.1177/1940161218781254'
+        },
+        contributors: {
+          contributor: [
+            {
+              "contributor-orcid": nil,
+              "credit-name": {
+                value: 'Nikki Usher'
+              },
+              "contributor-email": nil,
+              "contributor-attributes": {
+                "contributor-sequence": nil,
+                "contributor-role": 'author'
+              }
+            },
+            {
+              "contributor-orcid": nil,
+              "credit-name": {
+                value: 'Jesse Holcomb'
+              },
+              "contributor-email": nil,
+              "contributor-attributes": {
+                "contributor-sequence": nil,
+                "contributor-role": 'author'
+              }
+            },
+            {
+              "contributor-orcid": nil,
+              "credit-name": {
+                value: 'Justin Littman'
+              },
+              "contributor-email": nil,
+              "contributor-attributes": {
+                "contributor-sequence": nil,
+                "contributor-role": 'author'
+              }
+            }
+          ]
+        },
+        "language-code": nil,
+        country: nil
+      }
+    end
+
+    let(:put_code) { subject.add_work('https://sandbox.orcid.org/0000-0003-3437-349X', work, 'FAKE29cb-194e-4bc3-8afg-99315b06be04') }
+
+    context 'when creating work the first time' do
+      it 'adds works' do
+        VCR.use_cassette('Orcid_Client/_add_work/adds work') do
+          expect(put_code).to eq('1250170')
+        end
+      end
+    end
+
+    context 'when work already exists' do
+      it 'handles conflict' do
+        VCR.use_cassette('Orcid_Client/_add_work/adds work again') do
+          expect(put_code).to eq('1250170')
+        end
+      end
+    end
+  end
 end
