457d5ddeefe0b7cc1c9486baaeb0eed6ae3b3e10
Peter Mangiafico <peter@mangiafico.org>
2021-06-08 14:20:53 -0700

    enable cron schedule for orcid pushing in dev and uat

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,9 +22,15 @@ every 1.day, at: stagger(4), roles: [:harvester_dev, :harvester_qa, :harvester_p
   rake 'cap:poll[1]'
 end
 
-# poll mais for new ORCID information nightly at 5am-ish in qa and dev
+# poll mais for new ORCID information nightly at 5am-ish in prod, qa and dev
 every 1.day, at: stagger(5), roles: [:harvester_dev, :harvester_qa, :harvester_prod] do
   rake 'mais:update_authors'
+end
+
+# send publications to ORCID profiles for all authorized users at 6am-ish every 3 days in qa and dev
+# when validation is complete, we will add the :harvester_prod role and remove this comment
+every 3.days, at: stagger(6), roles: [:harvester_dev, :harvester_qa] do
+  rake 'orcid:add_all_works'
 end
 
 # ensure delayed_job is started on a reboot
