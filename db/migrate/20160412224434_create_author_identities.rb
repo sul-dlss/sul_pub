71316416728931590477f59e048941088d55de8b
Peter Mangiafico <peter@mangiafico.org>
2022-03-15 14:35:43 -0700

    adjust db migrations to work with sqlite3

--- db/migrate/20160412224434_create_author_identities.rb
+++ db/migrate/20160412224434_create_author_identities.rb
@@ -1,10 +1,9 @@
 class CreateAuthorIdentities < ActiveRecord::Migration[4.2]
   def change
     # On MySQL, foreign keys require the InnoDB storage engine, and we need to ensure UTF8 charsets for strings
-    opts = {}
-    opts[:options] = 'ENGINE=InnoDB DEFAULT CHARSET=utf8' if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
+    options = ActiveRecord::Base.connection.adapter_name.downcase.starts_with?('mysql') ? 'ENGINE=InnoDB DEFAULT CHARSET=utf8' : ''
 
-    create_table :author_identities, opts do |t|
+    create_table :author_identities, options: options do |t|
       t.belongs_to :author,       index: true, foreign_key: true, null: false
       t.integer :identity_type,   limit: 1, default: 0, null: false # enum: :alternate = 0, :official, :preferred, or :primary
       t.string :first_name,       limit: 255, null: false
--- db/migrate/20210520185112_create_orcid_source_records.rb
+++ db/migrate/20210520185112_create_orcid_source_records.rb
@@ -1,7 +1,8 @@
 class CreateOrcidSourceRecords < ActiveRecord::Migration[6.0]
+  # `:text` column with 16MB is the same as MySQL's MEDIUMTEXT but works on sqlite3
   def change
     create_table :orcid_source_records do |t|
-      t.mediumtext :source_data
+      t.text :source_data, limit: 16_777_215
       t.bigint :last_modified_date
       t.string :orcidid
       t.string :put_code
