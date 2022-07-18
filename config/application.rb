3a8851aaabdaa7663f50408a20a3665cf5140cfa
Aaron Collier <aaron.collier@stanford.edu>
2022-07-18 08:24:31 -0700

    Set yaml_column_permitted_classes due to 6.1.6.1 update

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Sulbib
     # the framework and any gems in your application.
 
     config.active_job.queue_adapter = :async
+
+    # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
+    config.active_record.yaml_column_permitted_classes = [Symbol]
   end
 end
