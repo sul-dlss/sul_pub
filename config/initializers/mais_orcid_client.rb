ac691076226ae06d12931605982ccc482e98436f
Laura Wrubel <lwrubel@stanford.edu>
2023-08-21 10:24:59 -0400

    Allow tests to run without authenticated client

--- config/initializers/mais_orcid_client.rb
+++ config/initializers/mais_orcid_client.rb
@@ -1,5 +1,13 @@
-MaisOrcidClient.configure(
-  client_id: Settings.MAIS.CLIENT_ID,
-  client_secret: Settings.MAIS.CLIENT_SECRET,
-  base_url: Settings.MAIS.BASE_URL
-)
+begin
+  MaisOrcidClient.configure(
+    client_id: Settings.MAIS.CLIENT_ID,
+    client_secret: Settings.MAIS.CLIENT_SECRET,
+    base_url: Settings.MAIS.BASE_URL
+  )
+rescue StandardError => e
+  # as of v0.3.1, mais_orcid_client tries to connect immediately upon configuration, which would
+  # prevent running tests or rails console on laptop.  would also prevent deployment or startup
+  # of sul_pub if configuration was incorrect (missing settings, stale password, etc).
+  Rails.logger.warn("Error configuring MaisOrcidClient: #{e}")
+  Honeybadger.notify(e)
+end
