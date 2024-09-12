d80a54dddac0cb400bd9c89655bb425294f56a3c
Peter Mangiafico <peter@mangiafico.org>
2024-09-12 15:33:58 -0700

    new temp environments

---
+++ config/deploy/qa-temp.rb
@@ -0,0 +1,8 @@
+# see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
+server 'sul-pub-cap-dev-temp.stanford.edu', user: 'pub', roles: %w(web db harvester_dev app)
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :rails_env, 'production'
+
+set :bundle_without, %w(development test).join(' ')
