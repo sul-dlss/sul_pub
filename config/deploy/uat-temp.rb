15b8f17b7d52c64d471731cd6a3fa2483acef780
Peter Mangiafico <peter@mangiafico.org>
2024-09-16 14:51:38 -0700

    New temporary UAT and dev environments for profiles testing (#1728)
    * new temp environments
    
    * remove harvesting from cron

---
+++ config/deploy/uat-temp.rb
@@ -0,0 +1,10 @@
+# see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
+server 'sul-pub-cap-uat-temp.stanford.edu', user: 'pub', roles: %w(web db app external_monitor)
+
+Capistrano::OneTimeKey.generate_one_time_key!
+
+set :rails_env, 'production'
+
+set :bundle_without, %w(test development).join(' ')
+
+set :log_level, :info
