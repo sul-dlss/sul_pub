025e10286d20e53d6712efa0b5384bb1fd3d4360
Peter Mangiafico <peter@mangiafico.org>
2025-01-17 16:14:29 -0800

    setup deploy for new prod-temp environment

--- config/deploy/uat-temp.rb
+++ config/deploy/uat-temp.rb
@@ -1,4 +1,6 @@
 # see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
+# see https://github.com/sul-dlss/operations-tasks/issues/3879
+# see https://docs.google.com/document/d/14K09NJG1O6Zo8u0LfrxK5g8ADLTBZ_HzkAnEr_YAsS8
 server 'sul-pub-cap-uat-temp.stanford.edu', user: 'pub', roles: %w(web db app harvester_uat external_monitor)
 
 Capistrano::OneTimeKey.generate_one_time_key!
