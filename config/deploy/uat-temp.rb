d4f18197aee461a2a67400dfc3d61fd4c1b4f93e
Peter Mangiafico <peter@mangiafico.org>
2025-03-04 08:16:25 -0800

    no need for harvesting in qa and deve temp (#1776)

--- config/deploy/uat-temp.rb
+++ config/deploy/uat-temp.rb
@@ -1,7 +1,7 @@
 # see https://github.com/sul-dlss/sul_pub/wiki/Servers-Deployment-environment
 # see https://github.com/sul-dlss/operations-tasks/issues/3879
 # see https://docs.google.com/document/d/14K09NJG1O6Zo8u0LfrxK5g8ADLTBZ_HzkAnEr_YAsS8
-server 'sul-pub-cap-uat-temp.stanford.edu', user: 'pub', roles: %w(web db app harvester_uat external_monitor)
+server 'sul-pub-cap-uat-temp.stanford.edu', user: 'pub', roles: %w(web db app external_monitor)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
