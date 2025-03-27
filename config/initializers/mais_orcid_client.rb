d39f6cc80cfbbc9fcc443583cee68325d43de51d
Peter Mangiafico <peter@mangiafico.org>
2025-03-27 16:55:46 -0700

    update mais-orcid-client gem

--- config/initializers/mais_orcid_client.rb
+++ config/initializers/mais_orcid_client.rb
@@ -2,7 +2,8 @@ begin
   MaisOrcidClient.configure(
     client_id: Settings.MAIS.CLIENT_ID,
     client_secret: Settings.MAIS.CLIENT_SECRET,
-    base_url: Settings.MAIS.BASE_URL
+    base_url: Settings.MAIS.BASE_URL,
+    token_url: Settings.MAIS.TOKEN_URL
   )
 rescue StandardError => e
   # as of v0.3.1, mais_orcid_client tries to connect immediately upon configuration, which would
